c94f9153ec3ace2a6acdd42c4e79392e2463fa4f
Jason Parham <bluemellophone@gmail.com>
2020-12-08 22:41:11 -0800

    Move all scripts to a scripts folder and add nightly, testing, and docker publish GitLab CI actioons

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./permissions.sh
+./scripts/permissions.sh
 
 docker rm -f houston
 docker run -d -it --publish 3000:5000 --name houston -v $(pwd)/_db/:/opt/houston/_db/ --restart unless-stopped wildme/houston:latest

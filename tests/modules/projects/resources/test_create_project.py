d344e6c399d92d5ac51a3a8790cc3ee0be1df7f5
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2020-12-18 15:25:08 +0000

    done some basic testing

---
+++ tests/modules/projects/resources/test_create_project.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import json
+
+
+def test_create_and_delete_project(flask_app_client, regular_user):
+    # pylint: disable=invalid-name
+
+    from app.modules.projects.models import Project
+
+    with flask_app_client.login(regular_user, auth_scopes=('projects:write',)):
+        response = flask_app_client.post(
+            '/api/v1/projects/',
+            data=json.dumps(
+                {
+                    'title': 'This is a test project, please ignore',
+                }
+            ),
+        )
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+    assert isinstance(response.json, dict)
+    assert set(response.json.keys()) >= {
+        'guid',
+        'title',
+    }
+
+    project_guid = response.json['guid']
+    read_project = Project.query.get(response.json['guid'])
+    assert read_project.title == 'This is a test project, please ignore'
+
+    # Try reading it back
+    with flask_app_client.login(regular_user, auth_scopes=('projects:read',)):
+        response = flask_app_client.get('/api/v1/projects/%s' % project_guid)
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+    assert isinstance(response.json, dict)
+    assert set(response.json.keys()) >= {
+        'guid',
+        'title',
+    }
+
+    # And deleting it
+    with flask_app_client.login(regular_user, auth_scopes=('projects:write',)):
+        response = flask_app_client.delete('/api/v1/projects/%s' % project_guid)
+
+    assert response.status_code == 204
+    read_project = Project.query.get(project_guid)
+    assert read_project is None

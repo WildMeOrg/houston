8f5c50d9de5d6ba0c46e2937f7a8c8efa9657d65
Jason Parham <bluemellophone@gmail.com>
2021-01-26 13:09:05 -0800

    Update links for WildbookOrg to WildMeOrg and fix Docker Hub push to namespace

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -22,7 +22,7 @@ REGISTRY=${REGISTRY:-}
 IMAGES=${@:-houston}
 # Set the image prefix
 if [ -n "$REGISTRY" ]; then
-    IMG_PREFIX="${REGISTRY}/wildbookorg/houston/"
+    IMG_PREFIX="${REGISTRY}/wildme/houston/"
 else
     IMG_PREFIX="wildme/"
 fi

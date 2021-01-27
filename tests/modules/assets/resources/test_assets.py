beb9527529c58f6595e478e7e4797095bdea9f8b
Jon Van Oast <jon@wildme.org>
2021-01-27 14:40:50 -0800

    order GET assets (list) api call lexographically by guid. hopefully fix failing test_assets as well.

--- tests/modules/assets/resources/test_assets.py
+++ tests/modules/assets/resources/test_assets.py
@@ -146,16 +146,13 @@ def test_read_all_assets(
         assert admin_response.status_code == 200
         assert admin_response.content_type == 'application/json'
         assert len(admin_response.json) == 2
-        # update: it seems this order received _is not_ deterministic
+        # admin_response.json should now be in lexographical order of guid
+        # still unsure if test_clone_submission_data is deterministic!  but these 2 appear to be also lexographical.
         assert (
-            test_clone_submission_data['asset_uuids'][0] == admin_response.json[0]['guid']
-            or test_clone_submission_data['asset_uuids'][0]
-            == admin_response.json[1]['guid']
+            admin_response.json[0]['guid'] == test_clone_submission_data['asset_uuids'][0]
         )
         assert (
-            test_clone_submission_data['asset_uuids'][1] == admin_response.json[0]['guid']
-            or test_clone_submission_data['asset_uuids'][1]
-            == admin_response.json[1]['guid']
+            admin_response.json[1]['guid'] == test_clone_submission_data['asset_uuids'][1]
         )
         assert regular_response.status_code == 403
 

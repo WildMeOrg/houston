7ca35dbfd1b2783c34dafe183e4883b91fcfaeda
Michael Mulich <michael.mulich@gmail.com>
2021-02-06 17:43:22 -0800

    Remove install redunancy
    `app.dependencies.install` is the grouping the two removed lines.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,4 @@ source virtualenv/houston3.7/bin/activate
 pip install -r requirements.txt
 pip install -e .
 
-invoke app.dependencies.install-python-dependencies
-invoke app.dependencies.install-swagger-ui
 invoke app.dependencies.install

9b84a972f1899d3ed2507f9baccb33831fed60c8
Colin Kingen <colinwkingen@gmail.com>
2021-02-17 15:59:28 -0800

    enable/test PATCH replace for Individual.encounters

--- app/modules/individuals/parameters.py
+++ app/modules/individuals/parameters.py
@@ -3,10 +3,21 @@
 Input arguments (Parameters) for Individuals resources RESTful API
 -----------------------------------------------------------
 """
-
 from flask_restplus_patched import Parameters, PatchJSONParameters
-
+from app.modules.users.permissions import rules
 from . import schemas
+import logging
+import uuid
+
+log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def is_valid_uuid(query_id):
+    try:
+        uuid.UUID(str(query_id))
+        return True
+    except ValueError:
+        return False
 
 
 class CreateIndividualParameters(Parameters, schemas.DetailedIndividualSchema):
@@ -20,3 +31,25 @@ class PatchIndividualDetailsParameters(PatchJSONParameters):
 
     VALID_FIELDS = ['encounters']
     PATH_CHOICES = tuple('/%s' % field for field in (VALID_FIELDS))
+
+    @classmethod
+    def replace(cls, obj, field, value, state):
+        ret_val = False
+        if field == 'encounters':
+            if len(value) == 0:
+                ret_val = True
+            encounter_list = []
+            for encounter_guid in value:
+                if is_valid_uuid(encounter_guid):
+                    from app.modules.encounters.models import Encounter
+
+                    encounter = Encounter.query.filter(
+                        Encounter.guid == encounter_guid
+                    ).first()
+                    if encounter is not None and encounter not in encounter_list:
+                        encounter_list.append(encounter)
+                    ret_val = True
+            obj.encounters = encounter_list
+        if ret_val is False:
+            super(PatchIndividualDetailsParameters, cls).replace(obj, field, value, state)
+        return ret_val
--- tests/modules/individuals/resources/test_individual_crud.py
+++ tests/modules/individuals/resources/test_individual_crud.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
 import logging
+import json
 
-from tests.modules.individuals.resources import utils as utils
+from tests.modules.individuals.resources import utils as individual_utils
+
+from tests import utils
 
 log = logging.getLogger(__name__)
 
@@ -13,7 +16,7 @@ def test_create_read_individual(flask_app_client, researcher_1, encounter_1):
 
     # TODO add checks for is_public
     encounter_1.owner = researcher_1
-    response = utils.create_individual(flask_app_client, researcher_1)
+    response = individual_utils.create_individual(flask_app_client, researcher_1)
 
     individual_guid = response.json['guid']
 
@@ -23,7 +26,9 @@ def test_create_read_individual(flask_app_client, researcher_1, encounter_1):
     read_individual.add_encounter(encounter_1)
     assert read_individual is not None
 
-    response = utils.read_individual(flask_app_client, researcher_1, individual_guid)
+    response = individual_utils.read_individual(
+        flask_app_client, researcher_1, individual_guid
+    )
     individual_guid = response.json['guid']
     read_individual = Individual.query.get(individual_guid)
     assert read_individual is not None
@@ -35,7 +40,76 @@ def test_read_failure_if_not_member_or_researcher(
     # User without an encounter member or researcher privileges cannot access.
     # Worth considering. Since actual individual metadata is minimal and encounters
     # control their own data access, there is little risk in exposing individual to all logged in users.
-    response = utils.create_individual(flask_app_client, researcher_1)
+    response = individual_utils.create_individual(flask_app_client, researcher_1)
     individual_guid = response.json['guid']
-    response = utils.read_individual(flask_app_client, regular_user, individual_guid, 403)
+    response = individual_utils.read_individual(
+        flask_app_client, regular_user, individual_guid, 403
+    )
     assert 'guid' not in response.json.items()
+
+
+def patch_individual(
+    flask_app_client, individual_guid, user, data, expected_status_code=200
+):
+    with flask_app_client.login(user, auth_scopes=('individuals:write',)):
+        response = flask_app_client.patch(
+            '%s%s' % ('/api/v1/individuals/', individual_guid),
+            content_type='application/json',
+            data=json.dumps(data),
+        )
+    if expected_status_code == 200:
+        utils.validate_dict_response(response, 200, {'guid'})
+    else:
+        utils.validate_dict_response(
+            response, expected_status_code, {'status', 'message'}
+        )
+    return response
+
+
+def test_modify_encounter(db, flask_app_client, researcher_1, empty_individual):
+    # pylint: disable=invalid-name
+    from app.modules.encounters.models import Encounter
+
+    # Had problems persisting and deleting fixture objects ¯\_(ツ)_/¯
+    # EX: sqlalchemy.exc.InvalidRequestError: Instance '<Encounter at 0x7f72df5b2700>' has been deleted.
+    mod_enc_1 = utils.generate_encounter_instance(
+        user_email='mod1@user', user_password='mod1user', user_full_name='Test User'
+    )
+    mod_enc_2 = utils.generate_encounter_instance(
+        user_email='mod2@user', user_password='mod2user', user_full_name='Test User'
+    )
+
+    with db.session.begin():
+        db.session.add(empty_individual)
+        db.session.add(mod_enc_1)
+        db.session.add(mod_enc_2)
+
+    encounters = [str(mod_enc_1.guid), str(mod_enc_2.guid)]
+
+    replace_encounters = [
+        utils.patch_replace_op('encounters', encounters),
+    ]
+
+    patch_individual(
+        flask_app_client,
+        '%s' % empty_individual.guid,
+        researcher_1,
+        replace_encounters,
+        200,
+    )
+    with db.session.begin():
+        db.session.refresh(empty_individual)
+        db.session.refresh(mod_enc_1)
+        db.session.refresh(mod_enc_2)
+
+    assert encounters[0] in [
+        str(encounter.guid) for encounter in empty_individual.get_encounters()
+    ]
+    assert encounters[1] in [
+        str(encounter.guid) for encounter in empty_individual.get_encounters()
+    ]
+
+    with db.session.begin():
+        db.session.delete(empty_individual)
+        db.session.delete(mod_enc_1)
+        db.session.delete(mod_enc_2)

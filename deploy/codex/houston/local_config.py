badedf2c06bf92ecfddcf83662f02e2e2c7e3335
Michael Mulich <michael.mulich@gmail.com>
2021-02-17 15:28:40 -0800

    Configure EDM via env-vars
    This allows for the definition of the EDM bits without writing to the
    configuration file.

--- deploy/codex/houston/local_config.py
+++ deploy/codex/houston/local_config.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from config import ProductionConfig as BaseConfig
 
 
@@ -6,3 +8,13 @@ class LocalConfig(BaseConfig):
     DEBUG = True
     SECRET_KEY = 'seekret'
     SENTRY_DSN = None
+
+    EDM_URIS = {
+        0: os.getenv('EDM_AUTHENTICATIONS_0_URI'),
+    }
+    EDM_AUTHENTICATIONS = {
+        0: {
+            'username' : os.getenv('EDM_AUTHENTICATIONS_0_USERNAME'),
+            'password' : os.getenv('EDM_AUTHENTICATIONS_0_PASSWORD'),
+        },
+    }

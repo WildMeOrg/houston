73aac4ecc8601b0df25ecddb266f988156f3ce14
Michael Mulich <michael.mulich@gmail.com>
2021-02-17 15:31:45 -0800

    Configure houston to use the data-root within a container volume
    DATA_ROOT is defined by the container at build time but can be
    overridden at runtime. This directory is by default within a
    volume. We redefine the location here in order to persist data between
    runs.

--- deploy/codex/houston/local_config.py
+++ deploy/codex/houston/local_config.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
+from pathlib import Path
 
 from config import ProductionConfig as BaseConfig
 
 
+DATA_ROOT = Path(os.getenv('DATA_ROOT', '/data/var'))
+
+
 class LocalConfig(BaseConfig):
     DEBUG = True
+
+    PROJECT_DATABASE_PATH = str(DATA_ROOT)
+    SUBMISSIONS_DATABASE_PATH = str(DATA_ROOT / 'submissions')
+    ASSET_DATABASE_PATH = str(DATA_ROOT / 'assets')
+    SQLALCHEMY_DATABASE_PATH = str(DATA_ROOT / 'database.sqlite3')
+
     SECRET_KEY = 'seekret'
     SENTRY_DSN = None
 

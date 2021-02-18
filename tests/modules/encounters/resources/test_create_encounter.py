fc7e3c7ee176a9886b54bf71a179ca9292eb39dd
Jon Van Oast <jon@wildme.org>
2021-02-17 17:43:40 -0800

    forgot my pre-commit grooming

--- tests/modules/encounters/resources/test_create_encounter.py
+++ tests/modules/encounters/resources/test_create_encounter.py
@@ -4,14 +4,12 @@
 from tests.modules.encounters.resources import utils as enc_utils
 
 
-#def test_create_and_delete_project(flask_app_client, anonymous_user_login):   researcher_1
+# def test_create_and_delete_project(flask_app_client, anonymous_user_login):   researcher_1
 def test_create_and_delete_encounter(flask_app_client, regular_user, admin_user):
     # pylint: disable=invalid-name
     from app.modules.encounters.models import Encounter
 
-    response = enc_utils.create_encounter(
-        flask_app_client, regular_user
-    )
+    response = enc_utils.create_encounter(flask_app_client, regular_user)
     assert response.status_code == 200
     enc_guid = response.json['result']['guid']
 
@@ -26,7 +24,7 @@ def test_create_and_delete_encounter(flask_app_client, regular_user, admin_user)
     assert response.json['locationId'] == 'PYTEST'
 
     # And deleting it
-    #enc_utils.delete_encounter(flask_app_client, admin_user, enc_guid)
+    # enc_utils.delete_encounter(flask_app_client, admin_user, enc_guid)
 
-    #read_enc = Encounter.query.get(enc_guid)
-    #assert read_enc is None
+    # read_enc = Encounter.query.get(enc_guid)
+    # assert read_enc is None
--- tests/modules/encounters/resources/utils.py
+++ tests/modules/encounters/resources/utils.py
@@ -11,10 +11,15 @@ PATH = '/api/v1/encounters/'
 
 def create_encounter(flask_app_client, user):
     with flask_app_client.login(user):
-        response = flask_app_client.post(PATH, data=json.dumps({'locationId': 'PYTEST'}), content_type='application/javascript')
+        response = flask_app_client.post(
+            PATH,
+            data=json.dumps({'locationId': 'PYTEST'}),
+            content_type='application/javascript',
+        )
 
     assert isinstance(response.json, dict)
     return response
+
 
 def read_encounter(flask_app_client, user, enc_guid):
     with flask_app_client.login(user, auth_scopes=('encounters:read',)):
@@ -23,10 +28,12 @@ def read_encounter(flask_app_client, user, enc_guid):
     assert isinstance(response.json, dict)
     return response
 
+
 def delete_encounter(flask_app_client, user, enc_guid, expected_status_code=204):
     with flask_app_client.login(user, auth_scopes=('encounter:delete',)):
         response = flask_app_client.delete('%s%s' % (PATH, enc_guid))
         import utool as ut
+
         ut.embed()
 
     if expected_status_code == 204:

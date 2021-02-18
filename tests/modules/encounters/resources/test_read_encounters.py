437475736c73583fb9dcbc4759dc135ffba8bbd4
Jon Van Oast <jon@wildme.org>
2021-02-18 13:49:14 -0800

    remove passthrough test (while decide the fate of them in general)

--- tests/modules/encounters/resources/test_read_encounters.py
+++ tests/modules/encounters/resources/test_read_encounters.py
@@ -10,16 +10,3 @@ def test_read_encounters(flask_app_client, researcher_1):
     with flask_app_client.login(researcher_1, auth_scopes=('encounters:read',)):
         response = flask_app_client.get(PATH)
     utils.validate_list_response(response, 200)
-
-
-def test_read_passthroughs(flask_app_client, researcher_1):
-    # pylint: disable=invalid-name
-    with flask_app_client.login(researcher_1, auth_scopes=('passthroughs:read',)):
-        site_name_response = flask_app_client.get(
-            '/api/v1/passthroughs/default/site-name'
-        )
-        garbage_response = flask_app_client.get('/api/v1/passthroughs/garbage/fubar')
-    # @jon what should this actually be returning. This test should be hitting the passthrough get
-    # functions and is not. Any ideas why not?
-    assert site_name_response.status_code == 200
-    assert garbage_response.status_code == 401

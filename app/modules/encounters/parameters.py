62fe7c3e07376f72dda2f7f084b0fefeda2cd7a2
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2021-02-25 06:39:57 +0000

    First asset addition patch

--- app/modules/encounters/parameters.py
+++ app/modules/encounters/parameters.py
@@ -20,10 +20,10 @@ class CreateEncounterParameters(Parameters, schemas.DetailedEncounterSchema):
 class PatchEncounterDetailsParameters(PatchJSONParametersWithPassword):
     # pylint: disable=abstract-method,missing-docstring
 
-    # Valid options for patching are '/owner'
+    # Valid options for patching are '/owner' and '/assetId'
     # The '/current_password' and user are not patchable but must be valid fields in the patch so that they can be
     # present for validation
-    VALID_FIELDS = ['current_password', 'user', 'owner']
+    VALID_FIELDS = ['current_password', 'user', 'owner', 'assetId']
     PATH_CHOICES = tuple('/%s' % field for field in VALID_FIELDS)
 
     @classmethod
@@ -33,7 +33,7 @@ class PatchEncounterDetailsParameters(PatchJSONParametersWithPassword):
         super(PatchEncounterDetailsParameters, cls).replace(obj, field, value, state)
         ret_val = False
         if field == 'owner':
-            # owner is permitted to assign project ownership to another researcher
+            # owner is permitted to assign ownership to another researcher
             user = User.query.get(value)
             if (
                 rules.owner_or_privileged(current_user, obj)
@@ -42,4 +42,24 @@ class PatchEncounterDetailsParameters(PatchJSONParametersWithPassword):
             ):
                 obj.owner = user
                 ret_val = True
+
+        return ret_val
+
+    @classmethod
+    def add(cls, obj, field, value, state):
+        from app.modules.assets.models import Asset
+
+        super(PatchEncounterDetailsParameters, cls).replace(obj, field, value, state)
+        ret_val = False
+
+        if field == 'assetId':
+            asset = Asset.query.get(value)
+            if (
+                rules.owner_or_privileged(current_user, obj)
+                and asset
+                and asset.owner is current_user
+            ):
+                obj.add_asset_in_context(asset)
+                ret_val = True
+
         return ret_val

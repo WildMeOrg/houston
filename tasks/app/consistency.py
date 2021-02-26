168cec392a95a9aeca79fe420de1b9246551f670
Jason Parham <bluemellophone@gmail.com>
2021-02-26 12:56:03 -0800

    Added new invoke command to cleanup GitLab repositories for TEST group

--- tasks/app/consistency.py
+++ tasks/app/consistency.py
@@ -9,6 +9,7 @@ import logging
 import tqdm
 
 from app.extensions import db
+from flask import current_app
 
 from ._utils import app_context_task
 
@@ -62,6 +63,56 @@ def user_staff_permissions(context):
                 updated += 1
 
     print('Updated %d users' % (updated,))
+
+
+@app_context_task
+def cleanup_gitlab(context, dryrun=False):
+    import gitlab
+
+    TEST_GROUP_NAMES = ['TEST']
+    PER_PAGE = 100
+    MAX_PAGES = 100
+
+    remote_uri = current_app.config.get('GITLAB_REMOTE_URI', None)
+    remote_personal_access_token = current_app.config.get(
+        'GITLAB_REMOTE_LOGIN_PAT', None
+    )
+
+    gl = gitlab.Gitlab(
+        remote_uri, private_token=remote_personal_access_token
+    )
+    gl.auth()
+    log.info('Logged in: %r' % (gl,))
+
+    projects = []
+    groups = gl.groups.list()
+    for group in groups:
+        if group.name in TEST_GROUP_NAMES:
+            page = 1
+            log.info('Fetching projects...')
+            for page in tqdm.tqdm(range(1, MAX_PAGES + 2), desc='Fetching GitLab Project Pages'):
+                projects_page = group.projects.list(per_page=PER_PAGE, page=page)
+
+                if len(projects_page) == 0:
+                    log.warn('Reached maximum page: %d' % (page, ))
+                    break
+                elif page == MAX_PAGES + 1:
+                    log.warn('More pages exist that were not processed')
+                    break
+                projects += projects_page
+    log.info('Fetched %d projects' % (len(projects), ))
+
+    if dryrun:
+        log.info('[DRYRUN] Would have deleted %d projects for groups %r' % (len(projects), TEST_GROUP_NAMES,))
+    else:
+        deleted = 0
+        for project in tqdm.tqdm(projects, desc='Deleting GitLab Projects'):
+            try:
+                gl.projects.delete(project.id)
+                deleted += 1
+            except gitlab.GitlabDeleteError:
+                pass
+        log.info('Deleted %d / %d projects for groups %r' % (deleted, len(projects), TEST_GROUP_NAMES,))
 
 
 @app_context_task

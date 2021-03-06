2d986f98130f69bdb31604c381825f9a0ed5c1c7
Michael Mulich <michael.mulich@gmail.com>
2021-03-05 18:47:14 -0800

    Circumvent issues with flask_restx Api object storing the app
    Changes the Api object to use a blueprint instead of waiting for the
    app to be given. This has two known benefits IMO. One, you ensure the
    Api object isn't deeply associated with a particular app instance,
    because the Api code has a design flaw where they store the app
    instance. Two, you can init the api in the exact location it's
    created.
    
    The intended goal of this change is towards a less global testing
    setup.

--- app/modules/api/__init__.py
+++ app/modules/api/__init__.py
@@ -3,18 +3,10 @@
 Houston API registration module
 ===============================
 """
-
-from flask import Blueprint
-
 from app.extensions import api
 
 
 def init_app(app, **kwargs):
-    # pylint: disable=unused-argument
-    api_v1_blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-    api.api_v1.init_app(api_v1_blueprint)
-    app.register_blueprint(api_v1_blueprint)
-
     # Touch underlying modules
     from . import resources
 

609057d6b8ad205e1dab08db320f1fcfbd9ed9a3
Jason Parham <bluemellophone@gmail.com>
2021-03-12 16:04:14 -0800

    Update UUIDs for task tests

--- scripts/run_tasks_for_coverage.sh
+++ scripts/run_tasks_for_coverage.sh
@@ -86,10 +86,10 @@ coverage run --append `which invoke` app.consistency.cleanup-gitlab --dryrun
 coverage run --append `which invoke` app.submissions.create-submission-from-path --path=tests/submissions/test-000/ || echo 'Expected failure'
 coverage run --append `which invoke` app.submissions.create-submission-from-path --path=tests/does-not-exist/ --email=user@example.org || echo 'Expected failure'
 coverage run --append `which invoke` app.submissions.create-submission-from-path --path=tests/submissions/test-000/ --email=user@example.org
-coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=4d7669ea-9fbe-4f87-be14-25b5391f7333 || echo 'Expected failure'
-coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=4d7669ea-0000-4f87-be14-25b5391f7333 --email=user@example.org || echo 'Expected failure'
-coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=4d7669ea-9fbe-4f87-be14-25b5391f7333 --email=user@example.org
-coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=4d7669ea-9fbe-4f87-be14-25b5391f7333 --email=user@example.org
+coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=00000000-0000-0000-0000-000000000002 || echo 'Expected failure'
+coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=11111111-1111-1111-1111-111111111111 --email=user@example.org || echo 'Expected failure'
+coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=00000000-0000-0000-0000-000000000002 --email=user@example.org
+coverage run --append `which invoke` app.submissions.clone-submission-from-gitlab --guid=00000000-0000-0000-0000-000000000002 --email=user@example.org
 coverage run --append `which invoke` app.submissions.list-all
 
 coverage run --append `which invoke` app.organizations.list-all

739d3f61d0c70ced5051c2948b993e7478688ad6
karen chan <karen@karen-chan.com>
2021-03-30 23:24:14 +0100

    Remove SQLALCHEMY_DATABASE_URI from docker local_config.py
    `SQLALCHEMY_DATABASE_URI` is already in `BaseConfig` and it reads from
    the environment variable `SQLALCHEMY_DATABASE_URI`.  In addition, if the
    environment variable `SQLALCHEMY_DATABASE_URI` is empty or not set, it
    would use sqlite in `_db/`.

--- deploy/codex/houston/local_config.py
+++ deploy/codex/houston/local_config.py
@@ -16,15 +16,13 @@ class LocalConfig(BaseConfig):
     SUBMISSIONS_DATABASE_PATH = str(DATA_ROOT / 'submissions')
     ASSET_DATABASE_PATH = str(DATA_ROOT / 'assets')
     UPLOADS_DATABASE_PATH = str(DATA_ROOT / 'uploads')
+    # FIXME: There is code that won't allow for `SQLALCHEMY_DATABASE_PATH = None`
+    #        File "/code/tasks/app/db.py", in upgrade: `if os.path.exists(_db_filepath):`
+    # SQLALCHEMY_DATABASE_PATH = None
     SQLALCHEMY_DATABASE_PATH = str(DATA_ROOT / 'database.sqlite3')
 
     SECRET_KEY = 'seekret'
     SENTRY_DSN = None
-
-    # FIXME: There is code that won't allow for `SQLALCHEMY_DATABASE_PATH = None`
-    #        File "/code/tasks/app/db.py", in upgrade: `if os.path.exists(_db_filepath):`
-    # SQLALCHEMY_DATABASE_PATH = None
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
     GITLAB_REMOTE_URI = os.getenv('GITLAB_REMOTE_URI')
     GITLAB_PUBLIC_NAME = os.getenv('GITLAB_PUBLIC_NAME')

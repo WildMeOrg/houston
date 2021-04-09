de2818ff03c6bb5a0b14d4ed56089ea0cd6f70cc
Michael Mulich <michael.mulich@gmail.com>
2021-04-08 17:55:28 -0700

    Revert "#122 reverting #118"
    This reverts commit 42a05a6809a4d24d22d45d6a62f109601868928a, reversing
    changes made to 10540640e4fbedda5171c8afe755284591f1d22c.

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=too-few-public-methods,invalid-name,missing-docstring
-import importlib
 import os
 import logging
 import datetime
@@ -21,36 +20,15 @@ _dotenv = os.getenv('HOUSTON_DOTENV', _DEFAULT_DOTENV)
 load_dotenv(_dotenv, override=False)  # gracefully fails if file doesn't exist
 
 PROJECT_ROOT = str(HERE)
-PROJECT_DATABASE_PATH = os.path.join(PROJECT_ROOT, '_db')
-
-# Load config from database folder
-SecretDevelopmentConfig = object
-SecretProductionConfig = object
-
-_config_filepath = os.path.join(PROJECT_DATABASE_PATH, 'secrets.py')
-if os.path.exists(_config_filepath):
-    _config_relative_filepath = os.path.relpath(_config_filepath)
-    _config_relative_filepath = _config_relative_filepath.strip('.py')
-    _config_relative_filepath = _config_relative_filepath.replace('/', '.')
-    _config_module = importlib.import_module(_config_relative_filepath)
-
-    secret_config = getattr(_config_module, 'SecretProductionConfig', None)
-    if secret_config is not None:
-        # log.info('Inheriting production secrets config %r' % (secret_config, ))
-        SecretProductionConfig = secret_config
-
-    secret_config = getattr(_config_module, 'SecretDevelopmentConfig', None)
-    if secret_config is not None:
-        # log.info('Inheriting development secrets config %r' % (secret_config, ))
-        SecretDevelopmentConfig = secret_config
+DATA_ROOT = Path(os.getenv('DATA_ROOT', HERE / '_db'))
 
 
 class BaseConfig(object):
     # SQLITE
     PROJECT_ROOT = PROJECT_ROOT
-    PROJECT_DATABASE_PATH = PROJECT_DATABASE_PATH
+    PROJECT_DATABASE_PATH = str(DATA_ROOT)
 
-    SUBMISSIONS_DATABASE_PATH = os.path.join(PROJECT_DATABASE_PATH, 'submissions')
+    SUBMISSIONS_DATABASE_PATH = str(DATA_ROOT / 'submissions')
     SUBMISSIONS_MIME_TYPE_WHITELIST = [
         'application/json',
         'application/ld+json',
@@ -80,7 +58,7 @@ class BaseConfig(object):
         'video/webm',
     ]
 
-    ASSET_DATABASE_PATH = os.path.join(PROJECT_DATABASE_PATH, 'assets')
+    ASSET_DATABASE_PATH = str(DATA_ROOT / 'assets')
     ASSET_ALLOWED_EXTS = [
         '.jpg',
         '.jpe',
@@ -94,12 +72,15 @@ class BaseConfig(object):
     ]
 
     # specifically this is where tus "temporary" files go
-    UPLOADS_DATABASE_PATH = os.path.join(PROJECT_DATABASE_PATH, 'uploads')
+    UPLOADS_DATABASE_PATH = str(DATA_ROOT / 'uploads')
     REDIS_HOST = os.getenv('REDIS_HOST') or 'localhost'
 
     FILEUPLOAD_BASE_PATH = os.path.join(PROJECT_DATABASE_PATH, 'fileuploads')
 
-    SQLALCHEMY_DATABASE_PATH = os.path.join(PROJECT_DATABASE_PATH, 'database.sqlite3')
+    # FIXME: There is code that won't allow for `SQLALCHEMY_DATABASE_PATH = None`
+    #        File "/code/tasks/app/db.py", in upgrade: `if os.path.exists(_db_filepath):`
+    # SQLALCHEMY_DATABASE_PATH = None
+    SQLALCHEMY_DATABASE_PATH = str(DATA_ROOT / 'database.sqlite3')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'sqlite:///%s' % (
         SQLALCHEMY_DATABASE_PATH
     )
@@ -109,6 +90,8 @@ class BaseConfig(object):
 
     PREFERRED_URL_SCHEME = 'http'
     REVERSE_PROXY_SETUP = os.getenv('HOSTON_REVERSE_PROXY_SETUP', False)
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     AUTHORIZATIONS = {
         'oauth2_password': {
@@ -200,6 +183,41 @@ class BaseConfig(object):
     }
 
 
+class EmailConfig(object):
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = bool(os.getenv('MAIL_USE_TLS', True))
+    MAIL_USE_SSL = bool(os.getenv('MAIL_USE_SSL', False))
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'dev@wildme.org')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'XXX')
+    MAIL_DEFAULT_SENDER = (
+        os.getenv('MAIL_DEFAULT_SENDER_NAME', 'Wild Me'),
+        os.getenv('MAIL_DEFAULT_SENDER_EMAIL', 'dev@wildme.org'),
+    )
+
+
+class ReCaptchaConfig(object):
+    RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY', 'XXX')
+    RECAPTCHA_BYPASS = os.getenv('RECAPTCHA_BYPASS', 'XXX')
+
+
+class StripeConfig(object):
+    STRIPE_PUBLIC_KEY = 'pk_test_XXX'
+    STRIPE_SECRET_KEY = 'sk_test_XXX'
+
+
+class GoogleAnalyticsConfig(object):
+    GOOGLE_ANALYTICS_TAG = 'G-XXX'
+
+
+class GoogleMapsConfig(object):
+    GOOGLE_MAP_API_KEY = 'XXX'
+
+
+class GoogleConfig(GoogleAnalyticsConfig, GoogleMapsConfig):
+    pass
+
+
 def get_env_rest_config(interface):
     """Parse ACM/EDM configuration from environment variables"""
     # Parse all uris from environment variables
@@ -259,7 +277,13 @@ class SubmissionGitLabRemoteConfig(object):
 
 
 class ProductionConfig(
-    BaseConfig, EDMConfig, ACMConfig, SubmissionGitLabRemoteConfig, SecretProductionConfig
+    BaseConfig,
+    EDMConfig,
+    ACMConfig,
+    EmailConfig,
+    GoogleConfig,
+    SubmissionGitLabRemoteConfig,
+    ReCaptchaConfig,
 ):
     TESTING = False
 
@@ -278,8 +302,10 @@ class DevelopmentConfig(
     BaseConfig,
     EDMConfig,
     ACMConfig,
+    EmailConfig,
+    GoogleConfig,
     SubmissionGitLabRemoteConfig,
-    SecretDevelopmentConfig,
+    ReCaptchaConfig,
 ):
     DEBUG = True
 

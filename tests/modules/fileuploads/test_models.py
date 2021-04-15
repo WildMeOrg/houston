4f98ec460fdc581f3cbe3f9a74da53f8bd1b423e
Jason Parham <bluemellophone@gmail.com>
2021-04-15 02:44:38 -0700

    Fix failing test with Python 3.7 and automatic cleanup of uploads in /api/v1/site-settings, related to an exit condition failure with tempfile.TemporaryDirectory

--- tests/modules/fileuploads/test_models.py
+++ tests/modules/fileuploads/test_models.py
@@ -5,11 +5,12 @@ import config
 import os
 import pathlib
 import shutil
-import tempfile
 
 from app.modules.fileuploads.models import FileUpload, modify_image
 from PIL import Image
 import pytest
+
+from tests.utils import TemporaryDirectoryGraceful
 
 
 def _source_file():
@@ -172,7 +173,7 @@ def test_fileuploads_get_src(flask_app, flask_app_client, db, request):
 
 
 def test_modify_image(flask_app):
-    with tempfile.TemporaryDirectory() as td:
+    with TemporaryDirectoryGraceful() as td:
         test_file = pathlib.Path(td) / 'zebra.jpg'
         with (
             pathlib.Path(flask_app.config.get('PROJECT_ROOT'))

37d75e3bf435e5d1276a921c3b76dc60aace2f02
Michael Mulich <michael.mulich@gmail.com>
2021-04-18 18:30:18 -0700

    make bash source compatible for both macos and linux
    This enables `source ./scripts/build.frontend.sh` to be used in both
    macos and linux.

--- scripts/build.frontend.sh
+++ scripts/build.frontend.sh
@@ -10,8 +10,9 @@ fi
 
 # check to see if this file is being run or sourced from another script
 function _is_sourced() {
-    # https://unix.stackexchange.com/a/215279
-    [ ${#BASH_SOURCE[@]} -eq 0 ]
+    # See also https://unix.stackexchange.com/a/215279
+    # macos bash source check OR { linux shell check }
+    [[ "${#BASH_SOURCE[@]}" -eq 0 ]] || { [ "${#FUNCNAME[@]}" -ge 2 ]  && [ "${FUNCNAME[0]}" = '_is_sourced' ] && [ "${FUNCNAME[1]}" = 'source' ]; }
 }
 
 function trap_error_help() {

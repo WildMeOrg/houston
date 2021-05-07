e33c01f206a635b1f54d32356c56d5e6604f548c
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2021-05-07 09:18:25 +0100

    correct fix for the migration error

--- app/modules/asset_groups/models.py
+++ app/modules/asset_groups/models.py
@@ -97,7 +97,7 @@ class AssetGroupJob(db.Model, HoustonModel):
     stage = db.Column(
         db.Enum(AssetGroupSightingStage),
         default=AssetGroupSightingStage.unknown,
-        nullable=True,
+        nullable=False,
     )
     owner_guid = db.Column(
         db.GUID,
@@ -115,7 +115,7 @@ class AssetGroupSighting(db.Model, HoustonModel):
     stage = db.Column(
         db.Enum(AssetGroupSightingStage),
         default=AssetGroupSightingStage.unknown,
-        nullable=True,
+        nullable=False,
     )
     owner_guid = db.Column(
         db.GUID,

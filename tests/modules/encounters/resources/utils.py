14a2483dd15aab150cfcc26ab8bd0543377ce05c
Jon Van Oast <jon@wildme.org>
2021-05-21 01:39:49 -0700

    modify util create_encounter() to do it properly, as part of a sighting

--- tests/modules/encounters/resources/utils.py
+++ tests/modules/encounters/resources/utils.py
@@ -3,27 +3,21 @@
 Project resources utils
 -------------
 """
-import json
 from tests import utils as test_utils
 
 PATH = '/api/v1/encounters/'
 
 
+# to create an encounter, it must be part of a sighting, so we piggyback on sighting_util
 def create_encounter(flask_app_client, user, expected_status_code=200):
-    if user is not None:
-        with flask_app_client.login(user):
-            response = flask_app_client.post(
-                PATH,
-                data=json.dumps({'locationId': 'PYTEST'}),
-                content_type='application/json',
-            )
-    else:
-        response = flask_app_client.post(
-            PATH,
-            data=json.dumps({'locationId': 'PYTEST'}),
-            content_type='application/json',
-        )
+    from tests.modules.sightings.resources import utils as sighting_utils
 
+    data_in = {
+        'locationId': 'PYTEST-SIGHTING',
+        'context': 'TEXT',
+        'encounters': [{'locationId': 'PYTEST-ENCOUNTER'}],
+    }
+    response = sighting_utils.create_sighting(flask_app_client, user, data_in)
     assert isinstance(response.json, dict)
     assert response.status_code == expected_status_code
     return response

8429e4b5932f5cde2c3e4da773de643334a16521
Jon Van Oast <jon@wildme.org>
2021-05-27 16:33:14 -0700

    some tests for keywords on annotations

--- tests/modules/annotations/resources/test_annotation_keywords.py
+++ tests/modules/annotations/resources/test_annotation_keywords.py
@@ -1,53 +1,103 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
 
-# import uuid
-# from tests import utils
+from tests import utils
 from tests.modules.annotations.resources import utils as annot_utils
-from tests.modules.asset_groups.resources import utils as sub_utils
-from tests.modules.encounters.resources import utils as enc_utils
+from tests.modules.keywords.resources import utils as keyword_utils
 
 
 def test_keywords_on_annotation(
-    flask_app_client, admin_user, researcher_1, test_clone_asset_group_data
+    flask_app_client, admin_user, researcher_1, test_clone_asset_group_data, db
 ):
     # pylint: disable=invalid-name
     from app.modules.annotations.models import Annotation
-    from app.modules.keywords.models import Keyword
+    from app.modules.keywords.models import Keyword, KeywordSource
 
     # this gives us an "existing" keyword to work with
-    keyword_value = 'TEST_VALUE_1'
-    keyword = Keyword(value=keyword_value)
+    keyword_value1 = 'TEST_VALUE_1'
+    keyword = Keyword(value=keyword_value1)
+    with db.session.begin():
+        db.session.add(keyword)
     assert keyword is not None
 
-    sub_utils.clone_asset_group(
-        flask_app_client,
-        admin_user,
-        researcher_1,
-        test_clone_asset_group_data['asset_group_uuid'],
-    )
-
-    # for now i guess we *must* have an encounter; but this likely will change
-    response = enc_utils.create_encounter(flask_app_client, researcher_1)
-    first_enc_guid = response.json['result']['guid']
-
-    response = annot_utils.create_annotation(
+    response = annot_utils.create_annotation_simple(
         flask_app_client,
         researcher_1,
         test_clone_asset_group_data['asset_uuids'][0],
-        first_enc_guid,
     )
 
     annotation_guid = response.json['guid']
     annotation = Annotation.query.get(annotation_guid)
     assert annotation is not None
 
-    return
-    # annot_utils.patch_annotation(
-    # flask_app_client, annotation_guid, researcher_1, move_to_second_enc
-    # )
+    # patch to add *existing* keyword
+    res = annot_utils.patch_annotation(
+        flask_app_client,
+        annotation.guid,
+        admin_user,
+        [utils.patch_add_op('keywords', str(keyword.guid))],
+    )
+    kw = annotation.get_keywords()
+    assert len(kw) == 1
+    assert kw[0].value == keyword_value1
+
+    # patch to add *new* keyword (by value)
+    keyword_value2 = 'TEST_VALUE_2'
+    res = annot_utils.patch_annotation(
+        flask_app_client,
+        annotation.guid,
+        admin_user,
+        [
+            utils.patch_add_op(
+                'keywords', {'value': keyword_value2, 'source': KeywordSource.user}
+            )
+        ],
+    )
+    kw = annotation.get_keywords()
+    assert len(kw) == 2
+    assert kw[1].value == keyword_value2
+
+    # patch to add *new* keyword (by value) -- except will fail (409) cuz keyword exists
+    res = annot_utils.patch_annotation(
+        flask_app_client,
+        annotation.guid,
+        admin_user,
+        [
+            utils.patch_add_op(
+                'keywords', {'value': keyword_value2, 'source': KeywordSource.user}
+            )
+        ],
+        409,
+    )
+
+    # patch to add invalid keyword guid
+    res = annot_utils.patch_annotation(
+        flask_app_client,
+        annotation.guid,
+        admin_user,
+        [utils.patch_add_op('keywords', '00000000-0000-0000-0000-000000002170')],
+        409,
+    )
+
+    # patch to remove a keyword (only can happen by id)
+    res = annot_utils.patch_annotation(
+        flask_app_client,
+        annotation.guid,
+        admin_user,
+        [utils.patch_remove_op('keywords', str(keyword.guid))],
+    )
+    kw = annotation.get_keywords()
+    assert len(kw) == 1
+    assert kw[0].value == keyword_value2
+
+    res = keyword_utils.read_all_keywords(flask_app_client, researcher_1)
+    assert len(res.json) == 2
 
     # And deleting it
-    annot_utils.delete_annotation(flask_app_client, researcher_1, annotation_guid)
+    annot_utils.delete_annotation(flask_app_client, admin_user, annotation_guid)
     read_annotation = Annotation.query.get(annotation_guid)
     assert read_annotation is None
+
+    # should still be the same... unless we are meant to cascade delete keywords?  FIXME
+    res = keyword_utils.read_all_keywords(flask_app_client, researcher_1)
+    assert len(res.json) == 2

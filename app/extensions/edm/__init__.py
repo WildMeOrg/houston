ea7fb8ba2e670b0583e3ac4ea2ee2cf7ec28ee41
Jon Van Oast <jon@wildme.org>
2021-06-03 10:53:55 -0700

    edm newer version

--- app/extensions/edm/__init__.py
+++ app/extensions/edm/__init__.py
@@ -35,7 +35,7 @@ class EDMManager(RestManager):
     ENDPOINT_PREFIX = 'api'
 
     # this is based on edm date of most recent commit (we must be at or greater than this)
-    MIN_VERSION = '2021-06-02 16:00:00 -0700'
+    MIN_VERSION = '2021-06-02 16:48:00 -0700'
 
     # We use // as a shorthand for prefix
     # fmt: off

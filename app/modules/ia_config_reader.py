df39c2edd145f892cd474ba119f150167d92b04a
Drew Blount <me@drew.computer>
2021-06-16 15:13:19 -0700

    adds tests

---
+++ app/modules/ia_config_reader.py
@@ -0,0 +1,115 @@
+from flask import Blueprint, Flask, current_app
+
+import logging
+import json
+import os.path as path
+
+import utool as ut
+
+log = logging.getLogger(__name__)
+
+
+def load_config_to_dict(fname):
+    app_home = ''
+    config_path = path.join(app_home, 'ia-configs', fname)
+    assert path.isfile(config_path), f'Could not find config at path {config_path}'
+    with open(config_path, 'r') as file:
+        config_dict = json.load(file)
+    return config_dict
+
+
+def short_config_name_to_full_filename(config_name):
+    fname = f'IA.{name}.json'
+    return fname
+
+
+class IaConfig:
+
+    def __init__(self, name="zebra"):
+        self.name = name
+        self.fname = short_config_name_to_full_filename(self.name)
+        self.config_dict = load_config_to_dict(self.fname)
+
+    def get(self, period_separated_keys):
+        keys = period_separated_keys.split('.')
+        return self.get_recursive(keys, self.config_dict)
+
+    def get_recursive(self, keys, config_dict_level):
+        print(f'get_recursive called on keys {keys}')
+        current_key = keys[0]
+        current_value = config_dict_level[current_key]
+        is_base_case = len(keys) == 1
+
+        # following @-links
+        if (_is_link(current_value)):
+            # resolve the link destination. It will look like an unlinked current_value.
+            current_value = self.get(_link_destination(current_value))
+        # base case, no link
+        if is_base_case:
+            value = current_value
+        # recursive case
+        else:
+            next_keys = keys[1:]
+            value = self.get_recursive(next_keys, current_value)
+        return value
+
+    # detects @-links in values from any level of the ia config
+    def _is_link(config_value):
+        is_link = type(config_value) is str and config_value.startswith('@')
+        return is_link
+
+    # simply takes the '@' off the beginning of a link
+    def _link_destination(link_str):
+        destination = link_str.split('@')[1]
+        return destination
+
+    def get_detectors_with_links(self, genus_species):
+        species_key = genus_species.replace(' ', '.')
+        detectors_key = f'{species_key}._detectors'
+        detectors = self.get(detectors_key)
+        return detectors
+
+    def get_detectors_list(self, genus_species):
+        detectors = self.get_detectors_with_links(genus_species)
+        detectors_list = self._resolve_links_in_value_list(detectors)
+        return detectors_list
+
+    def get_detectors_dict(self, genus_species):
+        detectors = self.get_detectors_with_links(genus_species)
+        detectors_dict = self._resolve_links_to_dict(detectors)
+        return detectors_dict
+
+    def get_identifiers_with_links(self, genus_species, ia_class):
+        species_key = genus_species.replace(' ', '.')
+        identifiers_key = f'{species_key}.{ia_class}._identifiers'
+        identifiers = self.get(identifiers_key)
+        return identifiers
+
+    def get_identifiers_list(self, genus_species, ia_class):
+        identifiers = self.get_identifiers_with_links(genus_species, ia_class)
+        identifiers_list = self._resolve_links_in_value_list(identifiers)
+        return identifiers_list
+
+    def get_identifiers_dict(self, genus_species, ia_class):
+        identifiers = self.get_identifiers_with_links(genus_species, ia_class)
+        identifiers_dict = self._resolve_links_to_dict(identifiers)
+        return identifiers_dict
+
+    def _resolve_links_in_value_list(self, value_list):
+        resolved_list = [
+            self.get(_link_destination(value))
+            if _is_link(value)
+            else value
+            for value in value_list
+        ]
+        return resolved_list
+
+    # takes a list of links, returns a dict mapping those links to their resolved values
+    def _resolve_links_to_dict(self, link_list):
+        resolved_dicts = {
+            _link_destination(link): self.get(_link_destination(link))
+            for link in link_list
+        }
+        return resolved_dicts
+
+    globals().update(locals())  # just for pasting into ipython
---
+++ tests/modules/test_ia_config.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from app.modules.ia_config_reader import IaConfig
+
+# note that these tests rely on the IA.zebra.json file, which if changed, might invalidate tests.
+TEST_CONFIG_NAME = 'zebra'
+
+
+def test_ia_config_creation(flask_app_client):
+
+    config_name = TEST_CONFIG_NAME
+    ia_config_reader = IaConfig(config_name)
+    assert ia_config_reader.name is config_name
+    assert ia_config_reader.fname.endswith('.json')
+    assert type(ia_config_reader.config_dict) is dict
+
+
+def test_at_links(flask_app_client):
+    ia_config_reader = IaConfig(TEST_CONFIG_NAME)
+    nolink_quagga_detectors = ia_config_reader.get_detectors_dict('Equus quagga')
+    # grevy config links to quagga config
+    linked_grevy_detectors = ia_config_reader.get_detectors_dict('Equus grevyi')
+    assert nolink_quagga_detectors == linked_grevy_detectors
+
+
+def test_detector_dict_vs_link(flask_app_client):
+    ia_config_reader = IaConfig(TEST_CONFIG_NAME)
+    species = 'Equus quagga'
+    detectors_list = ia_config_reader.get_detectors_dict(species)
+    detectors_dict = ia_config_reader.get_detectors_list(species)
+    detectors_dict_values = detectors_dict.values()
+
+    dict_items_in_list = [val in detectors_list for val in detectors_dict_values]
+    list_items_in_dict = [val in detectors_dict_values for val in detectors_list]
+
+    assert all(dict_items_in_list)
+    assert all(list_items_in_dict)
+
+
+def test_config_to_filename(flask_app_client):
+    from app.modules.ia_config_reader import short_config_name_to_full_filename
+    config_name = 'zebra'
+    filename = short_config_name_to_full_filename(config_name)
+    assert filename == 'IA.zebra.json'
+
+
+def test_loading_config_dict(flask_app_client):
+    from app.modules.ia_config_reader import short_config_name_to_full_filename
+    from app.modules.ia_config_reader import load_config_to_dict
+    config_name = 'zebra'
+    filename = short_config_name_to_full_filename(config_name)
+    config_dict = load_config_to_dict(filename)
+
+    assert type(config_dict) is dict
+
+    desired_keys = {'_README', '_detectors', '_identifiers', 'Equus'}
+    for key in desired_keys:
+        assert key in config_dict.keys()

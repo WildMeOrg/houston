09cb38a194fbc8e0081712a9a6090dd4afc3ac8d
karen chan <karen@karen-chan.com>
2021-08-31 16:29:11 +0100

    Add test for sighting.augment_edm_json
    Include a full json test for `sighting.augment_edm_json` to prepare for
    a change of implementation to marshmallow schema.

--- tests/modules/sightings/resources/test_custom_fields.py
+++ tests/modules/sightings/resources/test_custom_fields.py
@@ -6,7 +6,13 @@ from tests.extensions.edm import utils as edm_utils
 
 
 def test_custom_fields_on_sighting(
-    db, flask_app_client, researcher_1, test_root, staff_user, admin_user
+    db,
+    flask_app_client,
+    researcher_1,
+    test_root,
+    staff_user,
+    admin_user,
+    test_asset_group_uuid,
 ):
     from app.modules.sightings.models import Sighting
     import datetime
@@ -35,15 +41,71 @@ def test_custom_fields_on_sighting(
     sighting_id = response.json['result']['id']
     sighting = Sighting.query.get(sighting_id)
     assert sighting is not None
+
+    # Attach some assets
+    from app.modules.asset_groups.models import AssetGroup
+
+    assets = AssetGroup.query.get(test_asset_group_uuid).assets
+    sighting.add_assets(assets)
+
     full_sighting = sighting_utils.read_sighting(
         flask_app_client,
         researcher_1,
         sighting_id,
     )
+
+    encounters = [
+        {
+            'customFields': {},
+            'timeValues': [None, None, None, 0, 0],
+            'id': str(encounter.guid),
+            'version': encounter.version,
+            'createdHouston': encounter.created.isoformat(),
+            'updatedHouston': encounter.updated.isoformat(),
+            'owner': {
+                'full_name': encounter.owner.full_name,
+                'guid': str(encounter.owner.guid),
+                'profile_fileupload': None,
+            },
+            'submitter': {
+                'full_name': encounter.submitter.full_name,
+                'guid': str(encounter.submitter.guid),
+                'profile_fileupload': None,
+            },
+        }
+        for encounter in sighting.encounters
+    ]
+
+    assets = [
+        {
+            'guid': str(asset.guid),
+            'filename': asset.filename,
+            'src': asset.src,
+            'annotations': [],
+            'dimensions': asset.get_dimensions(),
+            'created': asset.created.isoformat() + '+00:00',
+            'updated': asset.updated.isoformat() + '+00:00',
+        }
+        for asset in sighting.get_assets()
+    ]
+
     # make sure customFields value is actually set
-    assert 'customFields' in full_sighting.json
-    assert cfd_id in full_sighting.json['customFields']
-    assert full_sighting.json['customFields'][cfd_id] == cfd_test_value
+    assert full_sighting.json == {
+        'id': str(sighting.guid),
+        'locationId': 'test',
+        'comments': 'None',
+        'encounters': encounters,
+        'encounterCounts': {'lifeStage': {}, 'sex': {}, 'individuals': 0},
+        'version': sighting.version,
+        'createdHouston': sighting.created.isoformat(),
+        'updatedHouston': sighting.updated.isoformat(),
+        'assets': assets,
+        'featuredAssetGuid': str(sighting.featured_asset_guid),
+        'customFields': {cfd_id: cfd_test_value},
+        # Only asserting that these fields exist
+        'startTime': full_sighting.json['startTime'],
+        'createdEDM': full_sighting.json['createdEDM'],
+    }
 
     # test patch on customFields
     new_cfd_test_value = 'NEW_CFD_TEST_VALUE'

6ac7947ba8eb54ef66d0de76f076460dc40b766e
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2021-09-14 14:48:20 +0100

    472 revoking read collaboration also revokes edit collaboration if approved

--- app/modules/collaborations/models.py
+++ app/modules/collaborations/models.py
@@ -230,6 +230,15 @@ class Collaboration(db.Model, HoustonModel):
                         association.read_approval_state, state
                     ):
                         association.read_approval_state = state
+                        # If a user revokes view and previously allowed edit, they automatically
+                        # revoke edit too
+                        if (
+                            state == CollaborationUserState.REVOKED
+                            and association.edit_approval_state
+                            == CollaborationUserState.APPROVED
+                        ):
+
+                            association.edit_approval_state = state
                         success = True
         return success
 

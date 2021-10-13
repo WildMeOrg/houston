4642cac7e188ed46eed0dbf59d31562956319360
Colin Kingen <colinwkingen@gmail.com>
2021-10-13 15:54:38 -0700

    complete Relationship model v0

--- app/modules/relationships/models.py
+++ app/modules/relationships/models.py
@@ -11,18 +11,28 @@ from datetime import datetime  # NOQA
 
 import uuid
 
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class RelationshipIndividualMember(db.Model, HoustonModel):
     relationship_guid = db.Column(
         db.GUID, db.ForeignKey('relationship.guid'), primary_key=True
     )
-    relationship = db.relationship(
-        'Relationship', back_populates='individual_members'
-    )
+    relationship = db.relationship('Relationship', back_populates='individual_members')
 
-    individual_guid = db.Column(db.GUID, db.ForeignKey('individual.guid'), primary_key=True)
+    individual_guid = db.Column(
+        db.GUID, db.ForeignKey('individual.guid'), primary_key=True
+    )
     individual = db.relationship('Individual', back_populates='relationships')
 
+    individual_role = db.Column(db.String, nullable=False)
+
+    def __init__(self, individual, individual_role, **kwargs):
+        self.individual = individual
+        self.individual_role = individual_role
+        self.individual_guid = individual.guid
 
 
 class Relationship(db.Model, Timestamp):
@@ -34,34 +44,46 @@ class Relationship(db.Model, Timestamp):
         db.GUID, default=uuid.uuid4, primary_key=True
     )  # pylint: disable=invalid-name
 
-    # individual_1_role = db.Column(db.String, nullable=False)
-
-    # individual_2_role = db.Column(db.String, nullable=False)
-
     individual_members = db.relationship(
         'RelationshipIndividualMember', back_populates='relationship'
     )
 
-    start_date = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=True)
+    start_date = db.Column(
+        db.DateTime, index=True, default=datetime.utcnow, nullable=True
+    )
     end_date = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=True)
 
-    def __init__(self, individual_1, individual_2, individual_1_role, individual_2_role, **kwargs):
-        self.individual_1 = individual_1
-        self.individual_2 = individual_2
-        self.individual_1_role = individual_1_role
-        self.individual_2_role = individual_2_role
+    type = db.Column(db.String, nullable=False)
 
+    def __init__(
+        self, individual_1, individual_2, individual_1_role, individual_2_role, **kwargs
+    ):
+        if individual_1 and individual_2 and individual_1_role and individual_2_role:
+
+            member_1 = RelationshipIndividualMember(individual_1, individual_1_role)
+            member_2 = RelationshipIndividualMember(individual_2, individual_2_role)
+
+            self.individual_members.append(member_1)
+            self.individual_members.append(member_2)
+        else:
+            raise ValueError('Relationship needs two individuals, each with a role.')
+
+    def has_individual(self, individual_guid):
+        for individual_member in self.individual_members:
+            if individual_member.individual_guid == individual_guid:
+                return True
+        return False
+
+    def get_relationship_role_for_individual(self, individual_guid):
+        if self.has_individual(individual_guid):
+            for individual_member in self.individual_members:
+                if individual_member.individual_guid is individual_guid:
+                    return individual_member.individual_role
+        return None
 
     def __repr__(self):
         return (
             '<{class_name}('
             'guid={self.guid}, '
-            'individual_1_guid={self.individual_1_guid}, '
-            'individual_2_guid={self.individual_2_guid}, '
-            'individual_1_role={self.individual_1_role}, '
-            'individual_2_role={self.individual_2_role}, '
-            ')>'.format(
-                class_name=self.__class__.__name__,
-                self=self
-            )
+            ')>'.format(class_name=self.__class__.__name__, self=self)
         )
--- tests/modules/relationships/test_relationship_model.py
+++ tests/modules/relationships/test_relationship_model.py
@@ -11,18 +11,22 @@ from app.modules.relationships.models import Relationship
 from tests.modules.individuals.resources import utils as individual_utils
 
 
-def test_relationship_instantiation(db, temp_user,):
+def test_relationship_instantiation():
 
     individual_1 = Individual()
     individual_2 = Individual()
 
-    relationship_1_role = "Mother"
-    relationship_2_role = "Calf"
+    relationship_role_1 = 'Mother'
+    relationship_role_2 = 'Calf'
 
-    relationship = Relationship(individual_1, individual_2, relationship_1_role, relationship_2_role)
+    relationship = Relationship(
+        individual_1, individual_2, relationship_role_1, relationship_role_2
+    )
 
-    assert relationship.individual_1 and relationship.individual_1 is individual_1
-    assert relationship.individual_2 and relationship.individual_2 is individual_2
+    assert relationship.has_individual(individual_1.guid)
+    assert relationship.has_individual(individual_2.guid)
 
-    assert relationship.individual_1_role and relationship.individual_1_role is individual_1
-    assert relationship.individual_2_role and relationship.individual_2_role is individual_2
+    assert (
+        relationship.get_relationship_role_for_individual(individual_1.guid) == 'Mother'
+    )
+    assert relationship.get_relationship_role_for_individual(individual_2.guid) == 'Calf'

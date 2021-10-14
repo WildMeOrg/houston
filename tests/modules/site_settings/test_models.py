d75ab7823d50403389aab7007dd651cfe6ebd48a
Jon Van Oast <jon@wildme.org>
2021-10-14 16:24:37 -0600

    lil more testing

--- tests/modules/site_settings/test_models.py
+++ tests/modules/site_settings/test_models.py
@@ -41,16 +41,24 @@ def test_create_string(db):
         db.session.delete(new_setting)
 
 
-def test_get_value():
+def test_get_value(db):
     # this should grab from edm and return a string
     val = SiteSetting.get_value('site.name')
     assert isinstance(val, str)
+
+    # test the default when no value
+    rnd = 'test_' + str(uuid.uuid4())
+    val = SiteSetting.get_value(rnd, default=rnd)
+    assert val == rnd
 
     guid = SiteSetting.get_system_guid()
     uuid.UUID(guid, version=4)  # will throw ValueError if not a uuid
     assert guid == SiteSetting.get_value('system_guid')
     # just for kicks lets test this too
     assert guid == SiteSetting.get_string('system_guid')
+    guid_setting = SiteSetting.query.get('system_guid')
+    assert guid_setting is not None
+    db.session.delete(guid_setting)
 
 
 def test_read_edm_configuration():

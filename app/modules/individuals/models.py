c280be9f635c470e4bc61c52561f3e594cd49d6c
Jon Van Oast <jon@wildme.org>
2021-10-16 00:08:42 -0600

    first pass at indiv.get_cooccurring_individuals() and indiv.get_shared_sightings(ind_guids)

--- app/modules/individuals/models.py
+++ app/modules/individuals/models.py
@@ -77,6 +77,71 @@ class Individual(db.Model, FeatherModel):
                 rt_val = self.encounters[0].annotations[0].asset_guid
         return rt_val
 
+    # returns Individuals
+    def get_cooccurring_individuals(self):
+        return Individual.get_multiple(self.get_cooccurring_individual_guids())
+
+    # returns guids
+    def get_cooccurring_individual_guids(self):
+        return Individual.get_cooccurring_individual_guids_for_individual_guid(self.guid)
+
+    # arbitrary individual_guid
+    @classmethod
+    def get_cooccurring_individual_guids_for_individual_guid(cls, individual_guid):
+        from app.modules.sightings.models import Sighting
+        from app.modules.encounters.models import Encounter
+        from sqlalchemy.orm import aliased
+
+        enc1 = aliased(Encounter, name='enc1')
+        enc2 = aliased(Encounter, name='enc2')
+        res = (
+            db.session.query(enc1.individual_guid)
+            .join(Sighting)
+            .join(enc2)
+            .filter(enc2.individual_guid == individual_guid)
+            .filter(enc1.individual_guid != individual_guid)
+            .group_by(enc1.individual_guid)
+        )
+        return [row.individual_guid for row in res]
+
+    # returns Sightings
+    def get_shared_sightings(self, individual_guids):
+        from app.modules.sightings.models import Sighting
+
+        return Sighting.get_multiple(self.get_shared_sighting_guids(individual_guids))
+
+    # returns guids
+    def get_shared_sighting_guids(self, individual_guids):
+        if (
+            not individual_guids
+            or not isinstance(individual_guids, list)
+            or len(individual_guids) < 1
+        ):
+            raise ValueError('must be passed a list of at least 1 individual guid')
+        individual_guids.append(self.guid)
+        return Individual.get_shared_sighting_guids_for_individual_guids(individual_guids)
+
+    @classmethod
+    def get_shared_sighting_guids_for_individual_guids(self, individual_guids):
+        if (
+            not individual_guids
+            or not isinstance(individual_guids, list)
+            or len(individual_guids) < 2
+        ):
+            raise ValueError('must be passed a list of at least 2 individual guids')
+        from app.modules.sightings.models import Sighting
+        from app.modules.encounters.models import Encounter
+        from sqlalchemy.orm import aliased
+
+        alias = []
+        res = db.session.query(Sighting.guid)
+        for i in range(len(individual_guids)):
+            alias[i] = aliased(Encounter, name=f'enc{i}')
+            res = res.join(alias[i])
+        for i in range(len(individual_guids)):
+            res = res.filter(alias[i].individual_guid == individual_guids[i])
+        return [row.guid for row in res]
+
     def set_featured_asset_guid(self, asset_guid):
         if self._ensure_asset_individual_association(asset_guid):
             self.featured_asset_guid = asset_guid

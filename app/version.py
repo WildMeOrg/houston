9be11e2da199522a243f362dd34c50f3ac2da66f
Michael Mulich <michael.mulich@gmail.com>
2021-11-18 12:38:36 -0800

    Fix development runtime by adding placeholder version.py module
    This module is required by the application, but in development it
    isn't necessary to be accurate. In a production build the contents of
    this file will get overwritten by the procedure in `setup.py`.

---
+++ app/version.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# THIS FILE IS GENERATED FROM SETUP.PY
+# If you are seeing this message, then this is a placeholder version
+# of the generated file. Run setup.py to write this file.
+version = '0.0.0'
+git_revision = ''
+full_version = version

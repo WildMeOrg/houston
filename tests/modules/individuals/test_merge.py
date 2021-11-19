6f69cdefd8c1e9dcf8bdf10035e3080c8b3ad6f6
Jon Van Oast <jon@wildme.org>
2021-11-19 16:49:23 -0700

    this test is no longer valid due to conflict changes

--- tests/modules/individuals/test_merge.py
+++ tests/modules/individuals/test_merge.py
@@ -40,7 +40,6 @@ def test_merge(db, flask_app_client, researcher_1, request):
     response = sighting_utils.create_sighting(flask_app_client, researcher_1, data_in)
     enc1_guid = response.json['result']['encounters'][0]['id']
     enc2_guid = response.json['result']['encounters'][1]['id']
-    enc3_guid = response.json['result']['encounters'][2]['id']
     enc1 = Encounter.query.get(enc1_guid)
     enc2 = Encounter.query.get(enc1_guid)
     sighting = Sighting.query.get(response.json['result']['id'])
@@ -94,33 +93,6 @@ def test_merge(db, flask_app_client, researcher_1, request):
     assert len(indiv1.encounters) == 2
     indiv2 = Individual.query.get(indiv2_guid)  # should be gone
     assert not indiv2
-
-    # now a 3rd indiv with sex=male
-    individual_data_in['sex'] = 'male'
-    individual_data_in['names']['defaultName'] = 'NAME3'
-    individual_data_in['encounters'][0]['id'] = enc3_guid
-    individual_response = individual_res_utils.create_individual(
-        flask_app_client, researcher_1, 200, individual_data_in
-    )
-    indiv3_guid = individual_response.json['result']['id']
-
-    indiv3 = Individual.query.get(indiv3_guid)
-    assert str(indiv3.encounters[0].guid) == enc3_guid
-    enc3 = Encounter.query.get(enc3_guid)
-    assert enc3.individual_guid == indiv3.guid
-    request.addfinalizer(enc3.delete_cascade)
-    request.addfinalizer(
-        lambda: individual_res_utils.delete_individual(
-            flask_app_client, researcher_1, indiv3.guid
-        )
-    )
-
-    resp = indiv1.merge_from(indiv3)
-    assert not resp.ok
-    assert resp.status_code == 500
-    json = resp.json()
-    assert 'errorFields' in json
-    assert 'sex' in json['errorFields']
 
 
 @pytest.mark.skipif(

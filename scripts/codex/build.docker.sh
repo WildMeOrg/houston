0f1ca638d5d79bd50a32c16dd40907ba2a8c9cfb
Michael Mulich <michael.mulich@gmail.com>
2021-11-19 12:13:35 -0800

    Define a single image for both codex and mws

--- scripts/codex/build.docker.sh
+++ scripts/codex/build.docker.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-# Use docker buildkit, because it allows for skipping unused stages
-# in a multi-stage build. We target using the 'main' build stage
-# and ignore the other build stages
-DOCKER_BUILDKIT=1 docker build --target main --tag wildme/codex:latest .
+../utils/build.docker.sh

c54a8f6e3b28d9e71b550a6d3326858ad39b5029
Jon Van Oast <jon@wildme.org>
2021-12-20 17:19:51 -0700

    a schema for detailed ComplexDateTime (currently unused)

---
+++ app/modules/complex_date_time/schemas.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Serialization schemas for ComplexDateTimes resources RESTful API
+----------------------------------------------------
+"""
+
+from flask_restx_patched import ModelSchema
+from flask_marshmallow import base_fields
+
+from .models import ComplexDateTime
+
+
+class BaseComplexDateTimeSchema(ModelSchema):
+    """
+    Base ComplexDateTime schema exposes only the most general fields.
+    """
+
+    timezoneNormalized = base_fields.Function(ComplexDateTime.get_timezone_normalized)
+    timeInTimezone = base_fields.Function(ComplexDateTime.isoformat_in_timezone)
+
+    class Meta:
+        # pylint: disable=missing-docstring
+        model = ComplexDateTime
+        fields = (
+            ComplexDateTime.guid.key,
+            ComplexDateTime.datetime.key,
+            ComplexDateTime.timezone.key,
+            ComplexDateTime.specificity.key,
+        )
+        dump_only = (ComplexDateTime.guid.key,)
+
+
+class DetailedComplexDateTimeSchema(BaseComplexDateTimeSchema):
+    """
+    Detailed ComplexDateTime schema exposes all useful fields.
+    """
+
+    class Meta(BaseComplexDateTimeSchema.Meta):
+        fields = BaseComplexDateTimeSchema.Meta.fields + (
+            'timezoneNormalized',
+            'timeInTimezone',
+        )
+        dump_only = BaseComplexDateTimeSchema.Meta.dump_only

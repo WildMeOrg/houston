0f61a5fe76a2cf67f5246c2bbb41ad59c426e0f8
Jason Parham <bluemellophone@gmail.com>
2022-01-06 04:56:49 -0800

    Add MWS attributes to Keyword module definition to support Asset tags

--- app/modules/job_control/models.py
+++ app/modules/job_control/models.py
@@ -2,10 +2,6 @@
 """Provides UI space served directly from this application"""
 import logging
 
-from app.modules.asset_groups.models import AssetGroupSighting
-from app.modules.sightings.models import Sighting
-
-
 log = logging.getLogger(__name__)
 
 
@@ -15,11 +11,21 @@ class JobControl(object):
     # Called by a periodic background task,
     @classmethod
     def check_jobs(cls):
+        from app.modules.asset_groups.models import AssetGroupSighting
+        from app.modules.sightings.models import Sighting
+        from app.modules.missions.models import Mission
+
         AssetGroupSighting.check_jobs()
         Sighting.check_jobs()
+        Mission.check_jobs()
 
     # Central point for all "job" related things to be accessed.
     @classmethod
     def print_jobs(cls):
+        from app.modules.asset_groups.models import AssetGroupSighting
+        from app.modules.sightings.models import Sighting
+        from app.modules.missions.models import Mission
+
         AssetGroupSighting.print_jobs()
         Sighting.print_jobs()
+        Mission.check_jobs()

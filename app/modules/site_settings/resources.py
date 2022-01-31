49f2a0cd74793bf4575b6ea7369fa77f94ee8450
Drew Blount <me@drew.computer>
2022-01-31 10:51:29 -0800

    minor linting fixes

--- app/modules/site_settings/resources.py
+++ app/modules/site_settings/resources.py
@@ -525,7 +525,7 @@ class DetectionConfigs(Resource):
         return response
 
 
-@detection_api.route('/ia_classes')
+@api.route('/ia_classes')
 class IaClassConfigs(Resource):
     """
     Configured IA classes
@@ -536,6 +536,8 @@ class IaClassConfigs(Resource):
         Returns a json describing the available detectors for the frontend to
         provide users with options
         """
+        from app.modules.ia_config_reader import IaConfig
+
         ia_config_reader = IaConfig()
         ia_classes = ia_config_reader.get_all_ia_classes()
         success = ia_classes is not None

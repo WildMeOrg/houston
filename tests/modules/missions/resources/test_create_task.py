e4f54828418d9cdd404a6d0963eb19bc7446a7f1
Jason Parham <bluemellophone@gmail.com>
2022-02-09 00:08:40 -0800

    Update Mission Task tests to check for Set Operation creation

--- tests/modules/missions/resources/test_create_task.py
+++ tests/modules/missions/resources/test_create_task.py
@@ -1,114 +1,243 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
 
+from tests import utils
 from tests.modules.missions.resources import utils as mission_utils
+from tests.utils import random_nonce, random_guid
+import tests.extensions.tus.utils as tus_utils
 import pytest
 
 from tests.utils import module_unavailable
 
 
 @pytest.mark.skipif(module_unavailable('missions'), reason='Missions module disabled')
-def test_create_and_delete_mission_task(flask_app_client, data_manager_1):
+def test_create_and_delete_mission_task(flask_app_client, data_manager_1, test_root):
     # pylint: disable=invalid-name
     from app.modules.missions.models import (
+        Mission,
+        MissionCollection,
         MissionTask,
         MissionTaskUserAssignment,
         MissionTaskAssetParticipation,
     )
 
-    response = mission_utils.create_mission(
-        flask_app_client, data_manager_1, 'This is a test mission, please ignore'
-    )
-    mission_guid = response.json['guid']
+    transaction_id, test_filename = tus_utils.prep_tus_dir(test_root)
+    mission_guid, mission_collection_guid = None, None
 
-    response = mission_utils.create_mission_task(
-        flask_app_client,
-        data_manager_1,
-        'This is a test task, please ignore',
-        mission_guid,
-    )
+    try:
+        response = mission_utils.create_mission(
+            flask_app_client, data_manager_1, 'This is a test mission, please ignore'
+        )
+        mission_guid = response.json['guid']
+        temp_mission = Mission.query.get(mission_guid)
 
-    mission_task_guid = response.json['guid']
-    read_mission_task = MissionTask.query.get(response.json['guid'])
-    assert read_mission_task.title == 'This is a test task, please ignore'
-    assert read_mission_task.owner == data_manager_1
+        previous_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
 
-    # Try reading it back
-    mission_utils.read_mission_task(flask_app_client, data_manager_1, mission_task_guid)
+        new_mission_collections = []
+        for index in range(3):
+            transaction_id = str(random_guid())
+            tus_utils.prep_tus_dir(test_root, transaction_id=transaction_id)
 
-    # And deleting it
-    mission_utils.delete_mission_task(flask_app_client, data_manager_1, mission_task_guid)
+            nonce = random_nonce(8)
+            description = 'This is a test mission collection (%s), please ignore' % (
+                nonce,
+            )
+            response = mission_utils.create_mission_collection_with_tus(
+                flask_app_client,
+                data_manager_1,
+                description,
+                transaction_id,
+                temp_mission.guid,
+            )
+            mission_collection_guid = response.json['guid']
+            temp_mission_collection = MissionCollection.query.get(mission_collection_guid)
 
-    read_mission_task = MissionTask.query.get(mission_task_guid)
-    assert read_mission_task is None
+            new_mission_collections.append((nonce, temp_mission_collection))
 
-    read_mission_task = MissionTaskUserAssignment.query.filter_by(
-        mission_task_guid=mission_task_guid
-    ).all()
-    assert len(read_mission_task) == 0
+        current_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
+        assert len(previous_list.json) + len(new_mission_collections) == len(
+            current_list.json
+        )
 
-    read_mission_task = MissionTaskAssetParticipation.query.filter_by(
-        mission_task_guid=mission_task_guid
-    ).all()
-    assert len(read_mission_task) == 0
+        nonce, new_mission_collection1 = new_mission_collections[0]
+        nonce, new_mission_collection2 = new_mission_collections[1]
+        data = [
+            utils.set_union_op('search', 'does-not-work'),
+            utils.set_difference_op('collections', [str(new_mission_collection1.guid)]),
+            utils.set_difference_op(
+                'assets', [str(new_mission_collection2.assets[0].guid)]
+            ),
+        ]
 
-    mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+        response = mission_utils.create_mission_task(
+            flask_app_client, data_manager_1, mission_guid, data
+        )
+
+        mission_task_guid = response.json['guid']
+        read_mission_task = MissionTask.query.get(response.json['guid'])
+        assert 'New Task: ' in read_mission_task.title
+        assert read_mission_task.owner == data_manager_1
+        mission_task_assets = read_mission_task.get_assets()
+        assert len(mission_task_assets) == 1
+        nonce, new_mission_collection3 = new_mission_collections[2]
+        assert mission_task_assets[0].git_store == new_mission_collection3
+
+        # Try reading it back
+        mission_utils.read_mission_task(
+            flask_app_client, data_manager_1, mission_task_guid
+        )
+
+        # And deleting it
+        mission_utils.delete_mission_task(
+            flask_app_client, data_manager_1, mission_task_guid
+        )
+
+        read_mission_task = MissionTask.query.get(mission_task_guid)
+        assert read_mission_task is None
+
+        read_mission_task = MissionTaskUserAssignment.query.filter_by(
+            mission_task_guid=mission_task_guid
+        ).all()
+        assert len(read_mission_task) == 0
+
+        read_mission_task = MissionTaskAssetParticipation.query.filter_by(
+            mission_task_guid=mission_task_guid
+        ).all()
+        assert len(read_mission_task) == 0
+    finally:
+        if mission_collection_guid:
+            mission_utils.delete_mission_collection(
+                flask_app_client, data_manager_1, mission_collection_guid
+            )
+        if mission_guid:
+            mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+        tus_utils.cleanup_tus_dir(transaction_id)
 
 
 @pytest.mark.skipif(module_unavailable('missions'), reason='Missions module disabled')
 def test_mission_task_permission(
-    flask_app_client, admin_user, staff_user, regular_user, data_manager_1, data_manager_2
+    flask_app_client,
+    admin_user,
+    staff_user,
+    regular_user,
+    data_manager_1,
+    data_manager_2,
+    test_root,
 ):
-    # Before we create any MissionTasks, find out how many are there already
-    previous_list = mission_utils.read_all_mission_tasks(flask_app_client, staff_user)
-
-    response = mission_utils.create_mission(
-        flask_app_client, data_manager_1, 'This is a test mission, please ignore'
-    )
-    mission_guid = response.json['guid']
-
-    response = mission_utils.create_mission_task(
-        flask_app_client,
-        data_manager_1,
-        'This is a test task, please ignore',
-        mission_guid,
+    from app.modules.missions.models import (
+        Mission,
+        MissionCollection,
     )
 
-    mission_task_guid = response.json['guid']
+    transaction_id, test_filename = tus_utils.prep_tus_dir(test_root)
+    mission_guid, mission_collection_guid = None, None
 
-    # staff user should be able to read anything
-    mission_utils.read_mission_task(flask_app_client, staff_user, mission_task_guid)
-    mission_utils.read_all_mission_tasks(flask_app_client, staff_user)
+    try:
+        response = mission_utils.create_mission(
+            flask_app_client, data_manager_1, 'This is a test mission, please ignore'
+        )
+        mission_guid = response.json['guid']
+        temp_mission = Mission.query.get(mission_guid)
 
-    # admin user should be able to read any tasks as well
-    mission_utils.read_mission_task(flask_app_client, admin_user, mission_task_guid)
-    mission_utils.read_all_mission_tasks(flask_app_client, admin_user)
+        previous_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
 
-    # user that created task can read it back plus the list
-    mission_utils.read_mission_task(flask_app_client, data_manager_1, mission_task_guid)
-    list_response = mission_utils.read_all_mission_tasks(flask_app_client, data_manager_1)
+        new_mission_collections = []
+        for index in range(3):
+            transaction_id = str(random_guid())
+            tus_utils.prep_tus_dir(test_root, transaction_id=transaction_id)
 
-    # due to the way the tests are run, there may be tasks left lying about,
-    # don't rely on there only being one
-    assert len(list_response.json) == len(previous_list.json) + 1
-    mission_task_present = False
-    for task in list_response.json:
-        if task['guid'] == mission_task_guid:
-            mission_task_present = True
-            break
-    assert mission_task_present
+            nonce = random_nonce(8)
+            description = 'This is a test mission collection (%s), please ignore' % (
+                nonce,
+            )
+            response = mission_utils.create_mission_collection_with_tus(
+                flask_app_client,
+                data_manager_1,
+                description,
+                transaction_id,
+                temp_mission.guid,
+            )
+            mission_collection_guid = response.json['guid']
+            temp_mission_collection = MissionCollection.query.get(mission_collection_guid)
 
-    # a different data manager should also be able to read the task
-    mission_utils.read_mission_task(flask_app_client, data_manager_2, mission_task_guid)
-    mission_utils.read_all_mission_tasks(flask_app_client, data_manager_2)
+            new_mission_collections.append((nonce, temp_mission_collection))
 
-    # but a regular user should not be able to read the list or the task
-    mission_utils.read_mission_task(
-        flask_app_client, regular_user, mission_task_guid, 403
-    )
-    mission_utils.read_all_mission_tasks(flask_app_client, regular_user, 403)
+        current_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
+        assert len(previous_list.json) + len(new_mission_collections) == len(
+            current_list.json
+        )
 
-    # delete it
-    mission_utils.delete_mission_task(flask_app_client, data_manager_1, mission_task_guid)
+        nonce, new_mission_collection1 = new_mission_collections[0]
+        nonce, new_mission_collection2 = new_mission_collections[1]
+        data = [
+            utils.set_union_op('search', 'does-not-work'),
+            utils.set_difference_op('collections', [str(new_mission_collection1.guid)]),
+            utils.set_difference_op(
+                'assets', [str(new_mission_collection2.assets[0].guid)]
+            ),
+        ]
 
-    mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+        response = mission_utils.create_mission_task(
+            flask_app_client, data_manager_1, mission_guid, data
+        )
+
+        mission_task_guid = response.json['guid']
+
+        # staff user should be able to read anything
+        mission_utils.read_mission_task(flask_app_client, staff_user, mission_task_guid)
+        mission_utils.read_all_mission_tasks(flask_app_client, staff_user)
+
+        # admin user should be able to read any tasks as well
+        mission_utils.read_mission_task(flask_app_client, admin_user, mission_task_guid)
+        mission_utils.read_all_mission_tasks(flask_app_client, admin_user)
+
+        # user that created task can read it back plus the list
+        mission_utils.read_mission_task(
+            flask_app_client, data_manager_1, mission_task_guid
+        )
+        list_response = mission_utils.read_all_mission_tasks(
+            flask_app_client, data_manager_1
+        )
+
+        # due to the way the tests are run, there may be tasks left lying about,
+        # don't rely on there only being one
+        assert len(list_response.json) == len(previous_list.json) + 1
+        mission_task_present = False
+        for task in list_response.json:
+            if task['guid'] == mission_task_guid:
+                mission_task_present = True
+                break
+        assert mission_task_present
+
+        # a different data manager should also be able to read the task
+        mission_utils.read_mission_task(
+            flask_app_client, data_manager_2, mission_task_guid
+        )
+        mission_utils.read_all_mission_tasks(flask_app_client, data_manager_2)
+
+        # but a regular user should not be able to read the list or the task
+        mission_utils.read_mission_task(
+            flask_app_client, regular_user, mission_task_guid, 403
+        )
+        mission_utils.read_all_mission_tasks(flask_app_client, regular_user, 403)
+
+        # delete it
+        mission_utils.delete_mission_task(
+            flask_app_client, data_manager_1, mission_task_guid
+        )
+    finally:
+        if mission_collection_guid:
+            mission_utils.delete_mission_collection(
+                flask_app_client, data_manager_1, mission_collection_guid
+            )
+        if mission_guid:
+            mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+        tus_utils.cleanup_tus_dir(transaction_id)
--- tests/modules/missions/resources/test_modify_task.py
+++ tests/modules/missions/resources/test_modify_task.py
@@ -3,118 +3,233 @@
 
 from tests import utils
 from tests.modules.missions.resources import utils as mission_utils
+from tests.utils import random_nonce, random_guid
+import tests.extensions.tus.utils as tus_utils
 import pytest
 
 from tests.utils import module_unavailable
 
 
 @pytest.mark.skipif(module_unavailable('missions'), reason='Missions module disabled')
-def test_modify_mission_task_users(db, flask_app_client, data_manager_1, data_manager_2):
-
+def test_modify_mission_task_users(
+    db, flask_app_client, data_manager_1, data_manager_2, test_root
+):
     # pylint: disable=invalid-name
-    from app.modules.missions.models import MissionTask
-
-    response = mission_utils.create_mission(
-        flask_app_client, data_manager_1, 'This is a test mission, please ignore'
+    from app.modules.missions.models import (
+        Mission,
+        MissionCollection,
+        MissionTask,
     )
-    mission_guid = response.json['guid']
 
-    response = mission_utils.create_mission_task(
-        flask_app_client,
-        data_manager_1,
-        'This is a test task, please ignore',
-        mission_guid,
-    )
-    mission_task_guid = response.json['guid']
+    transaction_id, test_filename = tus_utils.prep_tus_dir(test_root)
+    mission_guid, mission_collection_guid = None, None
 
-    mission_task = MissionTask.query.get(mission_task_guid)
-    assert len(mission_task.get_members()) == 1
+    try:
+        response = mission_utils.create_mission(
+            flask_app_client, data_manager_1, 'This is a test mission, please ignore'
+        )
+        mission_guid = response.json['guid']
+        temp_mission = Mission.query.get(mission_guid)
 
-    data = [
-        utils.patch_test_op(data_manager_1.password_secret),
-        utils.patch_add_op('user', '%s' % data_manager_2.guid),
-    ]
-    mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_1, data
-    )
-    assert len(mission_task.get_members()) == 2
+        previous_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
 
-    data = [
-        utils.patch_test_op(data_manager_1.password_secret),
-        utils.patch_remove_op('user', '%s' % data_manager_2.guid),
-    ]
-    mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_1, data
-    )
-    assert len(mission_task.get_members()) == 1
+        new_mission_collections = []
+        for index in range(3):
+            transaction_id = str(random_guid())
+            tus_utils.prep_tus_dir(test_root, transaction_id=transaction_id)
 
-    mission_utils.delete_mission_task(flask_app_client, data_manager_1, mission_task_guid)
+            nonce = random_nonce(8)
+            description = 'This is a test mission collection (%s), please ignore' % (
+                nonce,
+            )
+            response = mission_utils.create_mission_collection_with_tus(
+                flask_app_client,
+                data_manager_1,
+                description,
+                transaction_id,
+                temp_mission.guid,
+            )
+            mission_collection_guid = response.json['guid']
+            temp_mission_collection = MissionCollection.query.get(mission_collection_guid)
 
-    mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+            new_mission_collections.append((nonce, temp_mission_collection))
+
+        current_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
+        assert len(previous_list.json) + len(new_mission_collections) == len(
+            current_list.json
+        )
+
+        nonce, new_mission_collection1 = new_mission_collections[0]
+        nonce, new_mission_collection2 = new_mission_collections[1]
+        data = [
+            utils.set_union_op('search', 'does-not-work'),
+            utils.set_difference_op('collections', [str(new_mission_collection1.guid)]),
+            utils.set_difference_op(
+                'assets', [str(new_mission_collection2.assets[0].guid)]
+            ),
+        ]
+
+        response = mission_utils.create_mission_task(
+            flask_app_client, data_manager_1, mission_guid, data
+        )
+        mission_task_guid = response.json['guid']
+
+        mission_task = MissionTask.query.get(mission_task_guid)
+        assert len(mission_task.get_members()) == 1
+
+        data = [
+            utils.patch_test_op(data_manager_1.password_secret),
+            utils.patch_add_op('user', '%s' % data_manager_2.guid),
+        ]
+        mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_1, data
+        )
+        assert len(mission_task.get_members()) == 2
+
+        data = [
+            utils.patch_test_op(data_manager_1.password_secret),
+            utils.patch_remove_op('user', '%s' % data_manager_2.guid),
+        ]
+        mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_1, data
+        )
+        assert len(mission_task.get_members()) == 1
+
+        mission_utils.delete_mission_task(
+            flask_app_client, data_manager_1, mission_task_guid
+        )
+    finally:
+        if mission_collection_guid:
+            mission_utils.delete_mission_collection(
+                flask_app_client, data_manager_1, mission_collection_guid
+            )
+        if mission_guid:
+            mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+        tus_utils.cleanup_tus_dir(transaction_id)
 
 
 @pytest.mark.skipif(module_unavailable('missions'), reason='Missions module disabled')
-def test_owner_permission(flask_app_client, data_manager_1, data_manager_2):
-
-    response = mission_utils.create_mission(
-        flask_app_client, data_manager_1, 'This is a test mission, please ignore'
-    )
-    mission_guid = response.json['guid']
-
-    response = mission_utils.create_mission_task(
-        flask_app_client,
-        data_manager_1,
-        'This is a test task, please ignore',
-        mission_guid,
-    )
-    mission_task_guid = response.json['guid']
-
-    # another user cannot update the title
-    data = [
-        utils.patch_test_op(data_manager_2.password_secret),
-        utils.patch_add_op('title', 'Invalid update'),
-    ]
-    mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_2, data, 409
+def test_owner_permission(flask_app_client, data_manager_1, data_manager_2, test_root):
+    from app.modules.missions.models import (
+        Mission,
+        MissionCollection,
     )
 
-    # Owner can do that
-    data = [
-        utils.patch_test_op(data_manager_1.password_secret),
-        utils.patch_add_op('title', 'An owner modified test task, please ignore'),
-    ]
-    response = mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_1, data
-    )
-    assert response.json['title'] == 'An owner modified test task, please ignore'
+    transaction_id, test_filename = tus_utils.prep_tus_dir(test_root)
+    mission_guid, mission_collection_guid = None, None
 
-    # add data manager 2 user to the task
-    data = [
-        utils.patch_test_op(data_manager_1.password_secret),
-        utils.patch_add_op('user', '%s' % data_manager_2.guid),
-    ]
-    mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_1, data
-    )
+    try:
+        response = mission_utils.create_mission(
+            flask_app_client, data_manager_1, 'This is a test mission, please ignore'
+        )
+        mission_guid = response.json['guid']
+        temp_mission = Mission.query.get(mission_guid)
 
-    # make them the owner
-    data = [
-        utils.patch_test_op(data_manager_1.password_secret),
-        utils.patch_add_op('owner', '%s' % data_manager_2.guid),
-    ]
-    mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_1, data
-    )
+        previous_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
 
-    # try to remove a user as data manager 1, no longer owner, should fail
-    data = [
-        utils.patch_test_op(data_manager_1.password_secret),
-        utils.patch_remove_op('user', '%s' % data_manager_2.guid),
-    ]
-    mission_utils.patch_mission_task(
-        flask_app_client, mission_task_guid, data_manager_1, data, 409
-    )
+        new_mission_collections = []
+        for index in range(3):
+            transaction_id = str(random_guid())
+            tus_utils.prep_tus_dir(test_root, transaction_id=transaction_id)
 
-    mission_utils.delete_mission_task(flask_app_client, data_manager_1, mission_task_guid)
+            nonce = random_nonce(8)
+            description = 'This is a test mission collection (%s), please ignore' % (
+                nonce,
+            )
+            response = mission_utils.create_mission_collection_with_tus(
+                flask_app_client,
+                data_manager_1,
+                description,
+                transaction_id,
+                temp_mission.guid,
+            )
+            mission_collection_guid = response.json['guid']
+            temp_mission_collection = MissionCollection.query.get(mission_collection_guid)
 
-    mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+            new_mission_collections.append((nonce, temp_mission_collection))
+
+        current_list = mission_utils.read_all_mission_collections(
+            flask_app_client, data_manager_1
+        )
+        assert len(previous_list.json) + len(new_mission_collections) == len(
+            current_list.json
+        )
+
+        nonce, new_mission_collection1 = new_mission_collections[0]
+        nonce, new_mission_collection2 = new_mission_collections[1]
+        data = [
+            utils.set_union_op('search', 'does-not-work'),
+            utils.set_difference_op('collections', [str(new_mission_collection1.guid)]),
+            utils.set_difference_op(
+                'assets', [str(new_mission_collection2.assets[0].guid)]
+            ),
+        ]
+
+        response = mission_utils.create_mission_task(
+            flask_app_client, data_manager_1, mission_guid, data
+        )
+        mission_task_guid = response.json['guid']
+
+        # another user cannot update the title
+        data = [
+            utils.patch_test_op(data_manager_2.password_secret),
+            utils.patch_add_op('title', 'Invalid update'),
+        ]
+        mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_2, data, 409
+        )
+
+        # Owner can do that
+        data = [
+            utils.patch_test_op(data_manager_1.password_secret),
+            utils.patch_add_op('title', 'An owner modified test task, please ignore'),
+        ]
+        response = mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_1, data
+        )
+        assert response.json['title'] == 'An owner modified test task, please ignore'
+
+        # add data manager 2 user to the task
+        data = [
+            utils.patch_test_op(data_manager_1.password_secret),
+            utils.patch_add_op('user', '%s' % data_manager_2.guid),
+        ]
+        mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_1, data
+        )
+
+        # make them the owner
+        data = [
+            utils.patch_test_op(data_manager_1.password_secret),
+            utils.patch_add_op('owner', '%s' % data_manager_2.guid),
+        ]
+        mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_1, data
+        )
+
+        # try to remove a user as data manager 1, no longer owner, should fail
+        data = [
+            utils.patch_test_op(data_manager_1.password_secret),
+            utils.patch_remove_op('user', '%s' % data_manager_2.guid),
+        ]
+        mission_utils.patch_mission_task(
+            flask_app_client, mission_task_guid, data_manager_1, data, 409
+        )
+
+        mission_utils.delete_mission_task(
+            flask_app_client, data_manager_1, mission_task_guid
+        )
+    finally:
+        if mission_collection_guid:
+            mission_utils.delete_mission_collection(
+                flask_app_client, data_manager_1, mission_collection_guid
+            )
+        if mission_guid:
+            mission_utils.delete_mission(flask_app_client, data_manager_1, mission_guid)
+        tus_utils.cleanup_tus_dir(transaction_id)

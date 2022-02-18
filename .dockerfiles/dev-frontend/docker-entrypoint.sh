12b67d41f9241714205bc9a272496b52e9475c20
Michael Mulich <michael.mulich@gmail.com>
2022-02-18 11:41:07 -0800

    Mount all of Houston into frontend container
    In order to get the git information from the frontend code (e.g. run
    `git` commands in the `_frontend.codex` directory) the parent project
    must be accessible. This is only a problem because the `_frontend.*`
    repos are submodules; and thus not actually git repos
    themselves (i.e. it's complicated). This solves `fatal: No names
    found, cannot describe anything.` when running git commands within the
    submodule.
    
    This change is being made, because additions are being made to the
    frontend that require git info. The frontend build code discovers its
    own version information for display in the server-status page. This
    fix is required to enable that feature.

--- .dockerfiles/dev-frontend/docker-entrypoint.sh
+++ .dockerfiles/dev-frontend/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+cd /code/_frontend.${HOUSTON_APP_CONTEXT}
 npm install --legacy-peer-deps
 export NODE_ENV=development
 npx webpack serve --config ./config/webpack/webpack.common.js --host $HOST --public ${HOST}:${PORT} --env=houston=relative

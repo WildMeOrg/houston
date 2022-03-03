2385617547698798ea87a4899c6ef4fc65d0f378
Jason Parham <bluemellophone@gmail.com>
2022-03-03 00:22:24 -0800

    Add rich to test logging

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,9 @@
 The Application tests collection
 ================================
 """
+# Get all of the nice log formatting with Rich
+import tasks  # NOQA
+
 
 TEST_ASSET_GROUP_UUID = '00000000-0000-0000-0000-000000000003'
 TEST_EMPTY_ASSET_GROUP_UUID = '00000000-0000-0000-0000-000000000001'

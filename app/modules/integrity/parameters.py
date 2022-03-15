e33490d07d74be58f052c9450c91bb0cf154e37b
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2022-03-15 15:30:12 +0000

    added migration, tasks and fixed bug

--- app/modules/integrity/parameters.py
+++ app/modules/integrity/parameters.py
@@ -4,19 +4,11 @@ Input arguments (Parameters) for Integrity resources RESTful API
 -----------------------------------------------------------
 """
 
-from flask_restx_patched import Parameters, PatchJSONParameters
+from flask_restx_patched import Parameters
 
 from . import schemas
-from .models import Integrity
 
 
 class CreateIntegrityParameters(Parameters, schemas.DetailedIntegritySchema):
     class Meta(schemas.DetailedIntegritySchema.Meta):
         pass
-
-
-class PatchIntegrityDetailsParameters(PatchJSONParameters):
-    # pylint: disable=abstract-method,missing-docstring
-    OPERATION_CHOICES = (PatchJSONParameters.OP_REPLACE,)
-
-    PATH_CHOICES = tuple('/%s' % field for field in (Integrity.title.key,))

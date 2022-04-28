c260a9727611d42087b0ddf3ecc9adfa3c3260e3
karen chan <karen@karen-chan.com>
2022-04-28 12:07:44 +0100

    Add codex.job-control.check-sighting-jobs and .check-ags-jobs tasks

--- tasks/codex/job_control.py
+++ tasks/codex/job_control.py
@@ -37,3 +37,23 @@ def print_all_sighting_jobs(context, sighting_guid=None, verbose=True):
 
     for sighting in _get_jobs(Sighting, SightingStage.identification, sighting_guid):
         pprint.pprint(sighting.get_job_debug(None, verbose))
+
+
+@app_context_task()
+def check_ags_jobs(context, debug=False):
+    """AssetGroupSighting.check_jobs(), optional --debug to use pdb"""
+    from app.modules.asset_groups.models import AssetGroupSighting
+
+    if debug:
+        breakpoint()
+    AssetGroupSighting.check_jobs()
+
+
+@app_context_task()
+def check_sighting_jobs(context, debug=False):
+    """Sighting.check_jobs(), optional --debug to use pdb"""
+    from app.modules.sightings.models import Sighting
+
+    if debug:
+        breakpoint()
+    Sighting.check_jobs()

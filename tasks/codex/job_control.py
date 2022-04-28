670e2c360e6edc208cef7465cabbb1a4c6cbc202
karen chan <karen@karen-chan.com>
2022-04-28 12:00:57 +0100

    Change codex.job-control tasks to print all job details if guid not given
    `codex.job-control.print-all-ags-jobs` and
    `codex.job-control.print-all-sighting-jobs` tasks take options
    `--ags-guid` and `--sighting-guid` and if these are not given, print all
    the jobs of all "detection" and "identification" jobs.

--- tasks/codex/job_control.py
+++ tasks/codex/job_control.py
@@ -7,24 +7,33 @@ from tasks.utils import app_context_task
 import pprint
 
 
+def _get_jobs(model, stage, guid=None):
+    if guid:
+        items = model.query.filter(model.guid == guid)
+        if items.count() == 0:
+            print(f'{str(model)} {guid} not found')
+            return
+    else:
+        items = model.query.filter(model.stage == stage)
+    return items
+
+
 @app_context_task()
-def print_all_ags_jobs(context, ags_guid, verbose=True):
+def print_all_ags_jobs(context, ags_guid=None, verbose=True):
     """Print out the job status for all the detection jobs for the ags"""
-    from app.modules.asset_groups.models import AssetGroupSighting
+    from app.modules.asset_groups.models import (
+        AssetGroupSighting,
+        AssetGroupSightingStage,
+    )
 
-    ags = AssetGroupSighting.query.get(ags_guid)
-    if not ags:
-        print(f'AssetGroupSighting {ags_guid} not found')
-        return
-    pprint.pprint(ags.get_jobs_debug(verbose))
+    for ags in _get_jobs(AssetGroupSighting, AssetGroupSightingStage.detection, ags_guid):
+        pprint.pprint(ags.get_jobs_debug(verbose))
 
 
 @app_context_task()
-def print_all_sighting_jobs(context, sighting_guid, verbose=True):
+def print_all_sighting_jobs(context, sighting_guid=None, verbose=True):
     """Print out the job status for all the identification jobs for the sighting"""
-    from app.modules.sightings.models import Sighting
+    from app.modules.sightings.models import Sighting, SightingStage
 
-    sighting = Sighting.query.get(sighting_guid)
-    if not sighting:
-        print(f'Sighting {sighting_guid} not found')
-    pprint.pprint(sighting.get_job_debug(None, verbose))
+    for sighting in _get_jobs(Sighting, SightingStage.identification, sighting_guid):
+        pprint.pprint(sighting.get_job_debug(None, verbose))

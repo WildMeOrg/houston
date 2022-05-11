9b0f55bbcbd9b3c76b5af61d429a3575b65798c9
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2022-05-11 14:42:57 +0100

    some more testing changes.

--- app/modules/users/resources.py
+++ app/modules/users/resources.py
@@ -544,4 +544,12 @@ if is_module_enabled('asset_groups'):
             """
             Get AssetGroupSightings for user
             """
-            return user.get_unprocessed_asset_group_sightings()
+            from app.extensions.elapsed_time import ElapsedTime
+
+            timer = ElapsedTime()
+            all_ags = user.get_unprocessed_asset_group_sightings()
+            log.info(f'read of all ags {len(all_ags)} took {timer.elapsed()} secs')
+
+            result = AssetGroupSightingAsSightingSchema().dump(all_ags, many=True)
+            log.info(f'dump of all ags took {timer.elapsed()} secs')
+            return result

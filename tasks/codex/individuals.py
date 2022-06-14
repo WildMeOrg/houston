69aa3b4a27df41a16ac3b00146d43a030e63399d
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2022-06-14 14:05:40 +0100

    1006 individual pyinvoke
    votes util too
    
    util tweak for debug
    
    another fix
    
    1006 individual pyinvoke
    
    votes util too
    
    util tweak for debug
    
    another fix

---
+++ tasks/codex/individuals.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Application Individual related tasks for Invoke.
+"""
+
+from app.modules.individuals.models import Individual, IndividualMergeRequestVote
+from tasks.utils import app_context_task
+
+
+@app_context_task
+def list_all(context):
+    """
+    Show existing individuals.
+    """
+    individuals = Individual.query.all()
+
+    for individual in individuals:
+        print(f'Individual : {individual}')
+
+
+@app_context_task
+def details(context, guid, debug=False):
+    """
+    Show full existing of a specific Individual.
+
+    Command Line:
+    > invoke codex.individals.details 00000000-0000-0000-0000-000000000002
+    """
+    if debug:
+        breakpoint()
+
+    from app.modules.individuals.models import Individual
+    import pprint
+    individual = Individual.query.get(guid)
+
+    if individual is None:
+        print(f'Individual {guid} not found')
+        return
+
+    # Just reuse the debug schema
+    from app.modules.individuals.schemas import DebugIndividualSchema
+
+    schema = DebugIndividualSchema()
+    pprint.pprint(schema.dump(individual).data)
+
+
+@app_context_task
+def list_all_votes(context, debug=False):
+    if debug:
+        breakpoint()
+    all_votes = IndividualMergeRequestVote.query.all()
+    for vote in all_votes:
+        print(f'Vote : {vote}')

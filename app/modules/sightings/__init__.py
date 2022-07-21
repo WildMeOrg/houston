f0b72e38671dfb598a1bac526abb01c492920430
Jason Parham <bluemellophone@gmail.com>
2022-07-21 14:42:41 -0700

    Register models with Prometheus

--- app/modules/sightings/__init__.py
+++ app/modules/sightings/__init__.py
@@ -4,7 +4,7 @@ Sightings module
 ============
 """
 
-from app.extensions import register_elasticsearch_model
+from app.extensions import register_elasticsearch_model, register_prometheus_model
 from app.extensions.api import api_v1
 from app.modules import is_module_enabled
 
@@ -27,3 +27,4 @@ def init_app(app, **kwargs):
 
     # Register Models to use with Elasticsearch
     register_elasticsearch_model(models.Sighting)
+    register_prometheus_model(models.Sighting)

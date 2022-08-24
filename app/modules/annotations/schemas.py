a8bc450791f2e94e3ada2ff02248852b691581b1
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2022-08-24 11:34:00 +0100

    SCT-275 annotation refactoring

--- app/modules/annotations/schemas.py
+++ app/modules/annotations/schemas.py
@@ -6,7 +6,6 @@ Serialization schemas for Annotations resources RESTful API
 
 from flask_marshmallow import base_fields
 
-from app.modules import is_module_enabled
 from flask_restx_patched import ModelSchema
 
 from .models import Annotation
@@ -17,15 +16,15 @@ class BaseAnnotationSchema(ModelSchema):
     Base Annotation schema exposes only the most general fields.
     """
 
+    encounter_guid = base_fields.Function(lambda ann: ann.get_encounter_guid_str())
+
     class Meta:
         # pylint: disable=missing-docstring
         model = Annotation
         fields = (
             Annotation.guid.key,
             Annotation.asset_guid.key,
-            Annotation.encounter_guid.key
-            if is_module_enabled('encounters')
-            else 'encounter_guid',
+            'encounter_guid',
             Annotation.ia_class.key,
             Annotation.viewpoint.key,
             'elasticsearchable',
@@ -72,7 +71,6 @@ class AnnotationElasticsearchSchema(BaseAnnotationSchema):
     locationId = base_fields.Function(lambda ann: ann.get_location_id_str())
     taxonomy_guid = base_fields.Function(lambda ann: ann.get_taxonomy_guid_str())
     owner_guid = base_fields.Function(lambda ann: ann.get_owner_guid_str())
-    encounter_guid = base_fields.Function(lambda ann: ann.get_encounter_guid_str())
     sighting_guid = base_fields.Function(lambda ann: ann.get_sighting_guid_str())
     time = base_fields.Function(lambda ann: ann.get_time_isoformat_in_timezone())
 
@@ -86,7 +84,6 @@ class AnnotationElasticsearchSchema(BaseAnnotationSchema):
             'locationId',
             'owner_guid',
             'taxonomy_guid',
-            'encounter_guid',
             'sighting_guid',
             'time',
         )

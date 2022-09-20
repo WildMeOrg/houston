88af4a60f8d3de58a2725c64c4a4a19daa46d4b0
Howard Windsor <73688271+hwindsor@users.noreply.github.com>
2022-09-20 16:22:25 +0100

    1422 sage uri staff only

--- tests/modules/site_settings/resources/test_site_info.py
+++ tests/modules/site_settings/resources/test_site_info.py
@@ -5,6 +5,7 @@ import pytest
 from requests import Response
 
 import app.version
+from tests.modules.site_settings.resources import utils as site_setting_utils
 from tests.utils import extension_unavailable
 
 
@@ -22,7 +23,36 @@ def test_site_info(flask_app_client):
                 'version': '3.5.1.dev23',
             },
         }
-        resp = flask_app_client.get('/api/v1/site-settings/site-info/')
+        resp = site_setting_utils.read_site_info(flask_app_client, None)
+    assert resp.status_code == 200
+    assert resp.content_type == 'application/json'
+    assert resp.json == {
+        'houston': {
+            'version': app.version.version,
+            'git_version': app.version.git_revision,
+        },
+        'sage': {
+            'version': '3.5.1.dev23',
+            'uri': '',
+        },
+    }
+
+
+@pytest.mark.skipif(extension_unavailable('sage'), reason='Sage extension disabled')
+def test_site_info_staff(flask_app_client, staff_user):
+    with mock.patch('flask.current_app.sage.get_dict') as sage_get_dict:
+        sage_get_dict.return_value = {
+            'status': {
+                'success': True,
+                'code': 200,
+                'message': '',
+                'cache': -1,
+            },
+            'response': {
+                'version': '3.5.1.dev23',
+            },
+        }
+        resp = site_setting_utils.read_site_info(flask_app_client, staff_user)
     assert resp.status_code == 200
     assert resp.content_type == 'application/json'
     assert resp.json == {
@@ -44,7 +74,7 @@ def test_site_info_api_error(flask_app_client):
         not_found.status_code = 404
         sage_get_dict.return_value = not_found
         # sage returns 404
-        resp = flask_app_client.get('/api/v1/site-settings/site-info/')
+        resp = site_setting_utils.read_site_info(flask_app_client, None)
 
     assert resp.status_code == 200
     assert resp.content_type == 'application/json'
@@ -55,7 +85,7 @@ def test_site_info_api_error(flask_app_client):
         },
         'sage': {
             'version': '<Response [404]>',
-            'uri': 'http://sage:5000/',
+            'uri': '',
         },
     }
 

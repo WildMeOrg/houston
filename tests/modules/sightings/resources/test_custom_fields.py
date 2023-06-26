aa7e85bc8f3033fb3d2232975ce8cbe9d98f518a
Jon Van Oast <jon@wildme.org>
2023-06-26 16:48:54 -0600

    better testing of customFields on sightings

--- tests/modules/sightings/resources/test_custom_fields.py
+++ tests/modules/sightings/resources/test_custom_fields.py
@@ -26,6 +26,18 @@ def test_custom_fields_on_sighting(
         flask_app_client, admin_user, 'test_cfd', cls='Sighting'
     )
     assert cfd_id is not None
+    cfd_date_id = setting_utils.custom_field_create(
+        flask_app_client, admin_user, 'test_cfd_date', displayType='date', cls='Sighting'
+    )
+    assert cfd_date_id is not None
+    cfd_int_id = setting_utils.custom_field_create(
+        flask_app_client,
+        admin_user,
+        'test_cfd_integer',
+        displayType='integer',
+        cls='Sighting',
+    )
+    assert cfd_int_id is not None
     cfd_multi_id = setting_utils.custom_field_create(
         flask_app_client, admin_user, 'test_multi_cfd', multiple=True, cls='Sighting'
     )
@@ -35,6 +47,8 @@ def test_custom_fields_on_sighting(
     # transaction_id, test_filename = sighting_utils.prep_tus_dir(test_root)
     cfd_test_value = 'CFD_TEST_VALUE'
     cfd_multi_value = ['one', 'two']
+    cfd_date_value = '2000-01-02T03:04:05+00:00'
+    cfd_int_value = 123456
     data_in = {
         'time': timestamp,
         'timeSpecificity': 'time',
@@ -42,6 +56,8 @@ def test_custom_fields_on_sighting(
         'customFields': {
             cfd_id: cfd_test_value,
             cfd_multi_id: cfd_multi_value,
+            cfd_date_id: cfd_date_value,
+            cfd_int_id: cfd_int_value,
         },
         'encounters': [{}],
     }
@@ -116,6 +132,8 @@ def test_custom_fields_on_sighting(
             'customFields': {
                 cfd_id: cfd_test_value,
                 cfd_multi_id: cfd_multi_value,
+                cfd_date_id: cfd_date_value,
+                cfd_int_id: cfd_int_value,
             },
             # Only asserting that these fields exist
             'time': full_sighting.json['time'],
@@ -128,11 +146,17 @@ def test_custom_fields_on_sighting(
     assert 'customFields' in full_sighting.json
     assert cfd_id in full_sighting.json['customFields']
     assert cfd_multi_id in full_sighting.json['customFields']
+    assert cfd_int_id in full_sighting.json['customFields']
+    assert cfd_date_id in full_sighting.json['customFields']
     assert full_sighting.json['customFields'][cfd_id] == cfd_test_value
     assert full_sighting.json['customFields'][cfd_multi_id] == cfd_multi_value
+    assert full_sighting.json['customFields'][cfd_int_id] == cfd_int_value
+    assert full_sighting.json['customFields'][cfd_date_id] == cfd_date_value
 
     # test patch on customFields
     new_cfd_test_value = 'NEW_CFD_TEST_VALUE'
+    new_cfd_date_value = '2022-02-22T02:02:02.003000+02:00'  # the returned value gets these trailing zeroes added (003000), so we do same here
+    new_cfd_int_value = 99999
     new_cfd_multi_value = ['A', 'B']
     sighting_utils.patch_sighting(
         flask_app_client,
@@ -143,6 +167,15 @@ def test_custom_fields_on_sighting(
                 'op': 'replace',
                 'path': '/customFields',
                 'value': {'id': cfd_id, 'value': new_cfd_test_value},
+            },
+            # tests value flavor: { cfd_id1: value1, cfd_id2: value2 } which is what frontend actually uses
+            {
+                'op': 'replace',
+                'path': '/customFields',
+                'value': {
+                    cfd_date_id: new_cfd_date_value,
+                    cfd_int_id: new_cfd_int_value,
+                },
             },
             {
                 'op': 'replace',
@@ -164,6 +197,10 @@ def test_custom_fields_on_sighting(
     assert full_sighting.json['customFields'][cfd_id] == new_cfd_test_value
     assert cfd_multi_id in full_sighting.json['customFields']
     assert full_sighting.json['customFields'][cfd_multi_id] == new_cfd_multi_value
+    assert cfd_int_id in full_sighting.json['customFields']
+    assert full_sighting.json['customFields'][cfd_int_id] == new_cfd_int_value
+    assert cfd_date_id in full_sighting.json['customFields']
+    assert full_sighting.json['customFields'][cfd_date_id] == new_cfd_date_value
 
     # now since we have some values in use, lets make sure we cannot delete the definition
     res = setting_utils.patch_main_setting(

8cca8475cef0deacddccece40f60023e34098392
Jon Van Oast <jon@wildme.org>
2023-06-28 12:12:13 -0600

    gotta allow for false-feeling values passed

--- app/modules/individuals/parameters.py
+++ app/modules/individuals/parameters.py
@@ -274,7 +274,7 @@ class PatchIndividualDetailsParameters(PatchJSONParameters):
             # see explanation on encounter patch
             assert isinstance(value, dict), 'customFields must be passed a json object'
             if value.get('id'):
-                assert value.get('value'), 'customFields id/value format needs both'
+                assert 'value' in value, 'customFields id/value format needs both'
                 value = {value['id']: value['value']}
             obj.set_custom_field_values_json(value)  # does all the validation etc
             ret_val = True
--- app/modules/sightings/parameters.py
+++ app/modules/sightings/parameters.py
@@ -131,7 +131,7 @@ class PatchSightingDetailsParameters(PatchJSONParameters):
                     value, dict
                 ), 'customFields must be passed a json object'
                 if value.get('id'):
-                    assert value.get('value'), 'customFields id/value format needs both'
+                    assert 'value' in value, 'customFields id/value format needs both'
                     value = {value['id']: value['value']}
                 obj.set_custom_field_values_json(value)  # does all the validation etc
                 ret_val = True

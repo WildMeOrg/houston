5ef773929b8ea67ce4c208269a684487977718ac
veda-gogineni <vedagogineni98@gmail.com>
2024-02-27 11:09:25 -0800

    Test Case for Merge Functionality

--- app/modules/keywords/resources.py
+++ app/modules/keywords/resources.py
@@ -132,8 +132,6 @@ class KeywordByID(Resource):
         return None
 
 
-# @api.resolve_object_by_model(Keyword, 'keyword')
-#POST API: /api/v1/keyword/source_keyword_guid/target_keyword_guid/merge. I am getting 500 error when i am trying to run this api. How to retify this error?
 @api.route('/<uuid:source_keyword_guid>/<uuid:target_keyword_guid>/merge')
 @api.login_required(oauth_scopes=['keywords:write'])
 @api.response(
@@ -146,6 +144,7 @@ class MergeKeyword(Resource):
     """
     Merge source_keyword to target_keyword .
     """
+
     @api.permission_required(
         permissions.ModuleAccessPermission,
         kwargs_on_request=lambda kwargs: {
@@ -159,10 +158,10 @@ class MergeKeyword(Resource):
         Merge source_keyword to target_keyword .
         """
         try:
-            log.info("MergeKeyword: source_keyword: %s", source_keyword)
+            log.info('MergeKeyword: source_keyword: %s', source_keyword)
             target_keyword.merge(source_keyword)
 
         except Exception as e:
-            log.error("MergeKeyword: post: %s", e)
+            log.error('MergeKeyword: post: %s', e)
 
         return target_keyword
--- tests/modules/keywords/resources/test_keywords.py
+++ tests/modules/keywords/resources/test_keywords.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
+import uuid
+
 import tests.modules.keywords.resources.utils as keyword_utils
 from app.modules.keywords.models import Keyword
 from tests import utils as test_utils
 
 
 def test_get_keyword_not_found(flask_app_client, researcher_1):
-    import uuid
-
     keyword_utils.read_keyword(flask_app_client, None, str(uuid.uuid4()), 401)
     keyword_utils.read_keyword(flask_app_client, researcher_1, str(uuid.uuid4()), 404)
 
@@ -85,3 +85,30 @@ def test_modify_keyword(db, flask_app_client, researcher_1, staff_user):
     # should successfully kill this
     response = keyword_utils.delete_keyword(flask_app_client, staff_user, guid)
     assert orig_ct == test_utils.row_count(db, Keyword)
+
+
+def test_merge_keyword(db, flask_app_client, researcher_1, staff_user):
+    # Create two keywords
+    val1 = 'test_keyword_1'
+    response1 = keyword_utils.create_keyword(flask_app_client, researcher_1, val1)
+    guid1 = response1.json.get('guid', None)
+    assert guid1
+
+    val2 = 'test_keyword_2'
+    response2 = keyword_utils.create_keyword(flask_app_client, researcher_1, val2)
+    guid2 = response2.json.get('guid', None)
+    assert guid2
+
+    # Merge the two keywords
+    response = keyword_utils.merge_keyword(flask_app_client, staff_user, guid1, guid2)
+    assert response.status_code == 200
+
+    # Read the keyword with guid2, into which guid1 has been merged into
+    response = keyword_utils.read_keyword(flask_app_client, researcher_1, guid2)
+    # assert response.json.get('value', None) == val2
+    assert response.json.get('value', None) == val2
+
+    # Check that the first keyword (guid1) no longer exists
+    response = keyword_utils.read_keyword(
+        flask_app_client, researcher_1, guid1, expected_status_code=404
+    )
